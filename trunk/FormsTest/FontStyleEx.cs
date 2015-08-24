d17787bbec84b9e201cd2f57b559a8250b711faa
Jiri Volejnik <volejnik@emclient.com>
2015-08-24 16:00:44 +0000

    Troubleshooting layout issues: added more controls.

---
+++ trunk/FormsTest/FontStyleEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MailClient.Common.UI
+{
+    [Flags]
+    public enum FontStyleEx
+    {
+        // Summary:
+        //     Normal text.
+        Regular = 0,
+        //
+        // Summary:
+        //     Bold text.
+        Bold = 1,
+        //
+        // Summary:
+        //     Italic text.
+        Italic = 2,
+        //
+        // Summary:
+        //     Underlined text.
+        Underline = 4,
+        //
+        // Summary:
+        //     Text with a line through the middle.
+        Strikeout = 8,
+
+        Semibold = 16
+    }
+}
