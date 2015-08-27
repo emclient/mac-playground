a80bd2b83429a97688ce325074a3a239bb79a542
Jiri Volejnik <volejnik@emclient.com>
2015-08-27 15:12:14 +0000

    FlowLayout enhanced a bit (Mac only)

--- trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.Layout/FlowLayout.cs
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.Layout/FlowLayout.cs
@@ -93,7 +93,8 @@ namespace System.Windows.Forms.Layout
 
 				// Resize any AutoSize controls to their preferred size
 				if (c.AutoSize == true) {
-					Size new_size = c.GetPreferredSize (c.Size);
+					Size cSize = new Size (parent.ClientSize.Width, Int32.MaxValue);
+					Size new_size = c.GetPreferredSize (cSize);
 					c.SetBoundsInternal (c.Left, c.Top, new_size.Width, new_size.Height, BoundsSpecified.None);
 				}
 
