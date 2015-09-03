3fe16e627a634c7ed3ecb38812d45839bcc19eae
Jiri Volejnik <volejnik@emclient.com>
2015-09-03 10:19:02 +0000

    MacBridge and Macbridge.Test projects added to OXPlayground solution.

---
+++ trunk/MacBridge.Test/CGImageExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using MonoMac.CoreGraphics;
+using MacBridge.CoreGraphics;
+using System.Diagnostics;
+
+namespace MacBridge.Test
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[SetUp]
+		public void Setup()
+		{
+			Trace.Listeners.Add (new ConsoleTraceListener (true));
+		}
+
+		[Test ()]
+		public void TestCreateBitmapFromCGImage ()
+		{
+			// Load CGImage
+			var path =  @"../../data/SetupLogo.jpg";
+			Debug.WriteLine (System.IO.Path.GetFullPath (path));
+			var provider = new CGDataProvider (path);
+			var cgImage = CGImage.FromJPEG(provider, null, false, CGColorRenderingIntent.Default);
+
+			// Convert it to System.Drawing.Bitmap using Bitmap's internal constructor
+			var bitmap = cgImage.ToBitmap ();
+
+			Assert.AreEqual (cgImage.Width, bitmap.Width);
+		}
+	}
+}
+
---
+++ trunk/MacBridge/CGImageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.CoreGraphics;
+
+namespace MacBridge.CoreGraphics
+{
+	public static class CGImageExtensions
+	{
+		public static Bitmap ToBitmap(this CGImage cgImage)
+		{
+			return new Bitmap (cgImage);
+		}
+	}
+}
+
