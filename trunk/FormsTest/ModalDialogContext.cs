e4fe3abd4e9c144c9d9947402e2c1763d29e5578
Jiri Volejnik <volejnik@emclient.com>
2015-09-10 15:32:07 +0000

    Added skeletons for native versions of OpenFileDialog, SaveFileDIalog. ModalDialogContext class extracted into a single file.

---
+++ trunk/FormsTest/ModalDialogContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MonoMac.AppKit;
+using System.Windows.Forms;
+
+namespace FormsTest
+{
+	public class ModalDialogContext : IDisposable
+	{
+		NSWindow keyWindow;
+		NSResponder firstResponder;
+
+		public ModalDialogContext() {
+			keyWindow = NSApplication.SharedApplication.KeyWindow;
+			if (keyWindow != null)
+				firstResponder = keyWindow.FirstResponder;
+		}
+
+		public void Dispose ()
+		{
+			if (keyWindow != null) {
+				if (keyWindow.CanBecomeKeyWindow) {
+					try {
+						keyWindow.BecomeKeyWindow ();
+					} catch {
+					}
+				}
+				
+				if (firstResponder != null)
+					firstResponder.BecomeFirstResponder();
+			}
+		}
+	}
+}
+
