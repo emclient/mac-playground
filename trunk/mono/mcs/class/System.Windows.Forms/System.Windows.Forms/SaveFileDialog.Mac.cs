c0d18954eba7d9f669d4801c1f7a12a1984f7e19
Jiri Volejnik <volejnik@emclient.com>
2015-09-15 15:14:07 +0000

    Added SaveFileDialog.Mac.cs. FolderBrowserDialog inherited from FileDIalog.

---
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms/SaveFileDialog.Mac.cs
@@ -0,0 +1,50 @@
+﻿#if MONOMAC
+
+using System;
+using System.Windows.Forms;
+using System.Windows.Forms.CocoaInternal;
+using MonoMac.AppKit;
+
+namespace System.Windows.Forms
+{
+	public class SaveFileDialog : FileDialog
+	{
+		public string Description { get; set; }
+
+		// Gets or sets the root folder where the browsing starts from.
+		public Environment.SpecialFolder RootFolder { get; set; }
+
+		// Gets or sets the path selected by the user.
+		public string SelectedPath { get; set; }
+
+		// Gets or sets a value indicating whether the New Folder button appears in the folder browser dialog box.
+		public bool ShowNewFolderButton	{ get; set; }
+
+		public override void Reset()
+		{
+			SelectedPath = String.Empty;
+			Description = String.Empty;
+			RootFolder = Environment.SpecialFolder.Desktop;
+		}
+
+		protected override bool RunDialog (IntPtr hwndOwner)
+		{
+			return DialogResult.OK == ShowDialog();
+		}
+
+		public new DialogResult ShowDialog ()
+		{
+			using (var context = new ModalDialogContext ()) {
+				var panel = new MonoMac.AppKit.NSSavePanel ();
+				if (NSPanelButtonType.Ok == (NSPanelButtonType)panel.RunModal ()) {
+					SelectedPath = panel.Url.Path;
+					return DialogResult.OK;
+				}
+
+				return DialogResult.Cancel;
+			}
+		}
+	}
+}
+
+#endif //MONOMAC
