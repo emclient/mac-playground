3a2316da8e2caf0cf44bb5dbf01afdd103c5064e
Jiri Volejnik <aconcagua21@volny.cz>
2015-09-17 16:13:36 +0000

    Win32 skeletons added to MacBridge. Target of Mac projects set to .NET 4.0

---
+++ trunk/MacBridge/ComDlg32.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WinApi
+{
+    public class ComDlg32
+    {
+        public static bool ChooseFont(IntPtr lpcf)
+        {
+            // TODO:
+            return false;
+        }
+    }
+}
+
---
+++ trunk/MacBridge/Ole32.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinApi
+{
+    public static class Ole32
+    {
+        public static int RevokeDragDrop(IntPtr hwnd)
+        {
+            // TODO:
+            return 0;
+        }
+    }
+}
+
