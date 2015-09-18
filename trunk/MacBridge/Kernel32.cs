cfffa69b4fa74b113b64f118460192750b96ab9e
Jiri Volejnik <volejnik@emclient.com>
2015-09-18 18:18:56 +0000

    Added methods to MacBridge WinApi

---
+++ trunk/MacBridge/Kernel32.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WinApi
+{
+    public static partial class Win32
+    {
+        public static void GetStartupInfo([In, Out] STARTUPINFO lpStartupInfo)
+        {
+            // TODO
+            Debug.WriteLine(NotImplemented + MethodBase.GetCurrentMethod().Name);
+        }
+    }
+}
