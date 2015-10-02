16c8468f0806bef95d73df047d6222fdaad6dec2
Jiri Volejnik <volejnik@emclient.com>
2015-10-02 16:28:55 +0000

    Native MacOS calls wrapped with objects.

---
+++ trunk/MacBridge/MacApi/LaunchServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MonoMac.Foundation;
+
+namespace MacBridge.LaunchServices
+{
+    public partial class UTType
+    {
+        public static string CreatePreferredIdentifierForTag(string path)
+        {
+            var ext = new NSString(path).PathExtension;
+            var tagname = new NSString(kUTTagClassFilenameExtension);
+            var huti = UTTypeCreatePreferredIdentifierForTag(tagname.Handle, ext.Handle, IntPtr.Zero);
+            return NSString.FromHandle(huti);
+        }
+
+        #region Native API
+
+        const string LaunchServicesDll = "/system/Library/frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/LaunchServices";
+        const string kUTTagClassFilenameExtension = "public.filename-extension";
+
+        [DllImport(LaunchServicesDll)]
+        extern static IntPtr UTTypeCreatePreferredIdentifierForTag(IntPtr tagClass, IntPtr tag, IntPtr uti);
+
+        #endregion // Native API
+    }
+}
+
