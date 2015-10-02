27a325585944c5e300710294ce0bb04de697f004
Jiri Volejnik <aconcagua21@volny.cz>
2015-10-02 16:28:55 +0000

    Native MacOS calls wrapped with objects.

---
+++ trunk/MacBridge/MacApi/QuickLook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MonoMac.CoreGraphics;
+using MonoMac.Foundation;
+
+namespace MacBridge.QuickLook
+{
+    public static partial class QLThumbnailImage
+    {
+        public static CGImage Create(NSUrl url, CGSize maxThumbnailSize, bool asIcon)
+        {
+            var allocator = MonoMac.CoreFoundation.CFAllocator.Default;
+            var dict = NSDictionary.FromObjectAndKey (new NSNumber (asIcon), new NSString ("kQLThumbnailOptionIconModeKey"));
+            var cgImageHandle = QLThumbnailImageCreate(allocator.Handle, url.Handle, maxThumbnailSize, dict.Handle);
+            return cgImageHandle != IntPtr.Zero ? new CGImage(cgImageHandle) : null;
+        }
+
+        const string QuickLookDll = "/system/Library/frameworks/QuickLook.framework/QuickLook";
+
+        [DllImport(QuickLookDll)]
+        private extern static IntPtr QLThumbnailImageCreate(IntPtr allocator, IntPtr url, CGSize maxThumbnailSize, IntPtr dictionary); 
+    }
+}
+
