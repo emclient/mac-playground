b421899f3212119139c08d9e8e09f6931a28eb50
Jiri Volejnik <volejnik@emclient.com>
2015-10-23 16:18:17 +0000

    Test32 target replaced with eM Client 32

---
+++ trunk/Experiments/eM Client 32/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  eM Client 32
+//
+//  Created by Jiri Volejnik on 23/10/15.
+//  Copyright (c) 2015 eM Client. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+//@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
