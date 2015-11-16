6a3dfe04e1a405e70efc4161360b0437e483d5ee
Jiri Volejnik <volejnik@emclient.com>
2015-11-16 16:11:29 +0000

    Mac: Custom build shell scripts updated to use current installed version of Mono framework, not the build in Dependencies/Mac/... folder. Deleted redundant mono build from Dependencies/Mac/... folder.

---
+++ trunk/Experiments/eM Client 32/copy_app.sh
@@ -0,0 +1,2 @@
+cp -r "$PROJECT_DIR/../../../bin/x86/Mono - Debug/"* "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
+cp -r "$PROJECT_DIR/../../../bin/x86/Mono - Debug/".* "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
