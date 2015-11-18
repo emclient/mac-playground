2ce21c5e9438bda3ef405b0d710bc26c55d9725c
Jiri Volejnik <volejnik@emclient.com>
2015-11-18 13:03:56 +0000

    Mac: Added ConsoleTest project to test AOT. Removed link to "Mono - Debug" folder from eM Client 32.

--- trunk/Experiments/eM Client 32/copy_app.sh
+++ trunk/Experiments/eM Client 32/copy_app.sh
@@ -1,2 +1,13 @@
+export BIN_DIR="$PROJECT_DIR/../ConsoleTest/bin/Debug"
+#export BIN_DIR="$PROJECT_DIR/../../../bin/x86/Mono - Debug"
+export TMP_DIR="$PROJECT_DIR/../../../bin/x86/tmp"
+export OUT_DIR="$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
+
+cp -r "$BIN_DIR/" "$TMP_DIR/"
+#mono --aot -O=all "$TMP_DIR/Google.Apis.dll"
+mono --aot=full "$TMP_DIR/MailClient.exe"
+rm "$TMP_DIR/MailClient.exe"
+#mv "$TMP_DIR/MailClient.exe.config" "$TMP_DIR/MailClient.exe.dylib.config"
+
 mkdir -p "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
-cp -r "$PROJECT_DIR/../../../bin/x86/Mono - Debug/"* "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
+cp -r "$TMP_DIR/"* "$OUT_DIR"
