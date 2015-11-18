d89e35743f739d2fd0ff400488142d320603c022
Jiri Volejnik <volejnik@emclient.com>
2015-11-18 10:25:27 +0000

    Mac: Not needed mono libs removed from app bundle (commented out in build scripts)

--- trunk/Experiments/eM Client 32/copy_app.sh
+++ trunk/Experiments/eM Client 32/copy_app.sh
@@ -1,3 +1,2 @@
 mkdir -p "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
 cp -r "$PROJECT_DIR/../../../bin/x86/Mono - Debug/"* "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
-cp -r "$PROJECT_DIR/../../../bin/x86/Mono - Debug/".* "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
