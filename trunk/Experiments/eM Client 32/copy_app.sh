afb05180a63689738aab25a24ed9c3378767e1e9
Jiri Volejnik <volejnik@emclient.com>
2015-11-20 20:48:25 +0000

    copy_app script reverted

--- trunk/Experiments/eM Client 32/copy_app.sh
+++ trunk/Experiments/eM Client 32/copy_app.sh
@@ -1,13 +1,15 @@
-export BIN_DIR="$PROJECT_DIR/../ConsoleTest/bin/Debug"
-#export BIN_DIR="$PROJECT_DIR/../../../bin/x86/Mono - Debug"
-export TMP_DIR="$PROJECT_DIR/../../../bin/x86/tmp"
+export BIN_DIR="$PROJECT_DIR/../../../bin/x86/Mono - Debug"
 export OUT_DIR="$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
 
-cp -r "$BIN_DIR/" "$TMP_DIR/"
-#mono --aot -O=all "$TMP_DIR/Google.Apis.dll"
-mono --aot=full "$TMP_DIR/MailClient.exe"
-rm "$TMP_DIR/MailClient.exe"
-#mv "$TMP_DIR/MailClient.exe.config" "$TMP_DIR/MailClient.exe.dylib.config"
-
 mkdir -p "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
-cp -r "$TMP_DIR/"* "$OUT_DIR"
+cp -r "$BIN_DIR/"* "$OUT_DIR"
+
+#export BIN_DIR="$PROJECT_DIR/../ConsoleTest/bin/Debug"
+#export TMP_DIR="$PROJECT_DIR/../../../bin/x86/tmp"
+#export OUT_DIR="$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
+
+#cp -r "$BIN_DIR/" "$TMP_DIR/"
+#mono --aot -O=all "$TMP_DIR/Google.Apis.dll"
+#mono --aot=full "$TMP_DIR/MailClient.exe"
+#rm "$TMP_DIR/MailClient.exe"
+#mv "$TMP_DIR/MailClient.exe.config" "$TMP_DIR/MailClient.exe.dylib.config"
