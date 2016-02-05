021fcb0f1f7fa069efdfce61ce69e690194a9f07
Jiri Volejnik <volejnik@emclient.com>
2016-02-05 14:40:40 +0000

    Mac compatibility: Imm32 decls added to MacBridge project (prevnts crashes when typing in edit box on Mac).

---
+++ trunk/MacBridge/WinApi/Imm32.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace WinApi
+{
+	public static partial class Win32
+	{
+		public static IntPtr ImmGetContext(IntPtr hWnd)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return IntPtr.Zero;
+		}
+
+		public static IntPtr ImmAssociateContext(IntPtr hWnd, IntPtr hIMC)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return IntPtr.Zero;
+		}
+
+		public static int ImmGetCompositionStringW(IntPtr hIMC, int dwIndex, byte[] lpBuf, int dwBufLen)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return 0;
+		}
+
+		public static int ImmGetCompositionStringW(IntPtr hIMC, int dwIndex, uint[] lpBuf, int dwBufLen)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return 0;
+		}
+
+		public static int ImmSetCandidateWindow(IntPtr himc, ref CANDIDATEFORM lpCandidateForm)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return 0;
+		}
+
+	}
+}
