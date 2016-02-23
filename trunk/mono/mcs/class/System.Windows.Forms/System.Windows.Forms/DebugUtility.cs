5ebc1fbdb606108366e96c44fd3adf8ef20076d3
Jiri Volejnik <aconcagua21@volny.cz>
2016-02-23 18:35:57 +0000

    Mac: Added DebugUtility class

---
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms/DebugUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms
+{
+	internal static class DebugUtility
+	{
+		internal static String ControlInfo(IntPtr handle)
+		{
+			var ctrl = Control.FromHandle(handle);
+			return ctrl == null ? "null" : ctrl.GetType().Name + ", hwnd=" + handle + "visible=" + ctrl.Visible;
+		}
+	}
+}
+
