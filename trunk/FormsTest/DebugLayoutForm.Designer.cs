2b9a093785196c7071b06a921694127340ececdc
Greg Sutton <sutton@emclient.com>
2016-04-20 15:21:52 +0000

    Layout debugging

--- trunk/FormsTest/DebugLayoutForm.Designer.cs
+++ trunk/FormsTest/DebugLayoutForm.Designer.cs
@@ -35,7 +35,9 @@
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(10);
             this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
             this.panel1.Size = new System.Drawing.Size(100, 100);
             this.panel1.TabIndex = 0;
             // 
