7914b5d691067b39e81b1c3852d567c4d3548f25
Jiri Volejnik <aconcagua21@volny.cz>
2016-04-21 14:21:23 +0000

    DebugUtility enhanced - added looking up field names for the whole control hierarchy.

--- trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms/DebugUtility.cs
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms/DebugUtility.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace System.Windows.Forms
 {
-	internal static class DebugUtility
+	public static class DebugUtility
 	{
-		internal static String ControlInfo(Control ctrl)
+		public static String ControlInfo(Control ctrl)
 		{
-			return ctrl == null ? "null" : ctrl.GetType().Name + ", visible=" + ctrl.Visible + ", WxH=" + ctrl.Size.Width + "x" + ctrl.Size.Height;
+			return ctrl == null 
+				? "null"
+				: ctrl.GetType().Name + ", visible=" + ctrl.Visible + ", WxH=" + ctrl.Size.Width + "x" + ctrl.Size.Height
+				      + ", XY=" + ctrl.Location.X + "," + ctrl.Location.Y + ", text=\"" + ctrl.Text
+				      + "\"" + GetFieldInfo(ctrl);
 		}
 
-		internal static String ControlInfo(IntPtr handle)
+		public static String ControlInfo(IntPtr handle)
 		{
 			return ControlInfo(Control.FromHandle(handle));
 		}
 
-		internal static String ControlInfo(MonoMac.AppKit.NSView view)
+		public static String ControlInfo(MonoMac.AppKit.NSView view)
 		{
 			var ctrl = ControlFromView(view);
 			if (ctrl != null)
@@ -27,14 +32,14 @@ namespace System.Windows.Forms
 		}
 
 		static String lastInfo;
-		internal static void WriteInfoIfChanged(MonoMac.AppKit.NSView view)
+		public static void WriteInfoIfChanged(MonoMac.AppKit.NSView view)
 		{
 			string info = ControlInfo(view);
 			if (info != lastInfo)
 				Console.WriteLine(lastInfo = info);
 		}
 
-		internal static void WriteInfoIfChanged(IntPtr handle)
+		public static void WriteInfoIfChanged(IntPtr handle)
 		{
 			var c = Control.FromHandle(handle);
 			string info = ControlInfo(handle);
@@ -42,10 +47,62 @@ namespace System.Windows.Forms
 				Console.WriteLine(lastInfo = info);
 		}
 
-		internal static Control ControlFromView(MonoMac.AppKit.NSView view)
+		public static Control ControlFromView(MonoMac.AppKit.NSView view)
 		{
 			if (view != null && view is CocoaInternal.MonoView)
 				return Control.FromHandle(view.Handle);
+			return null;
+		}
+
+		public static bool SwitchingConversations = false;
+
+		public static string GetFieldInfo(Control control, string indent = "  ", string newLine = null)
+		{
+			newLine = newLine ?? Environment.NewLine.ToString();
+
+			var text = "";
+			for (var c = control; c != null; c = c.Parent)
+			{
+				var field = ReverseLookupField(c);
+				if (field == null)
+					break;
+
+				text = newLine + indent + field.Name + ":" + c.GetType().Name +  text;
+			}
+
+			var form = control.FindForm();
+			if (form != null)
+				text = newLine + indent + form.GetType().Name + text;
+
+			return text;
+		}
+
+		public static FieldInfo ReverseLookupField(Control control)
+		{
+			for (var parent = control.Parent; parent != null; parent = parent.Parent)
+			{
+				var field = FindField(parent, control);
+				if (field != null)
+					return field;
+			}
+			return null;
+		}
+
+		public static FieldInfo FindField(Control parent, Control control)
+		{
+			if (parent == null || control == null)
+				return null;
+
+			var type = parent.GetType();
+			var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+
+			foreach (var field in fields)
+			{
+				var value = field.GetValue(parent);
+				if (value != null && Object.ReferenceEquals(value, control))
+					return field;
+			}
+
 			return null;
 		}
 	}
