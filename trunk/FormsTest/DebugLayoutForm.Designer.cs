0ab446f1297d56d36fb71a3ea545b7d1737fdd81
Greg Sutton <sutton@emclient.com>
2016-04-25 09:47:00 +0000

    try turning off auto scale to font

--- trunk/FormsTest/DebugLayoutForm.Designer.cs
+++ trunk/FormsTest/DebugLayoutForm.Designer.cs
@@ -43,8 +43,7 @@
             // 
             // DebugLayoutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(484, 361);
             this.Controls.Add(this.panel1);
             this.Name = "DebugLayoutForm";
