0eaa2ae82f67a5efa137780fb8b0d8bab8cecdbf
Jiri Volejnik <aconcagua21@volny.cz>
2016-05-03 11:42:00 +0000

    Default menu creation moved from XplatUICocoa to MonoApplicationDelegate. Fixes issue with overwriting app delegate with the default one, which enables calling c# event handlers.

--- trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/MonoApplicationDelegate.cs
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/MonoApplicationDelegate.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using MonoMac.AppKit;
+﻿using MonoMac.AppKit;
+using MonoMac.Foundation;
+using MonoMac.ObjCRuntime;
 
 namespace System.Windows.Forms.CocoaInternal
 {
@@ -12,16 +13,18 @@ namespace System.Windows.Forms.CocoaInternal
 			this.driver = driver;
 		}
 
-		public override void DidBecomeActive (MonoMac.Foundation.NSNotification notification)
+		public override void DidBecomeActive (NSNotification notification)
 		{
 			foreach (NSWindow utility_window in XplatUICocoa.UtilityWindows)
 				if (!utility_window.IsVisible)
 					utility_window.OrderFront (utility_window);
 
-			driver.PostMessage (XplatUI.GetActive (), Msg.WM_ACTIVATEAPP, (IntPtr)WindowActiveFlags.WA_ACTIVE, IntPtr.Zero);
+			driver.SendMessage (XplatUI.GetActive (), Msg.WM_ACTIVATEAPP, (IntPtr)WindowActiveFlags.WA_ACTIVE, IntPtr.Zero);
+
+			CreateMenu();
 		}
 
-		public override void DidResignActive (MonoMac.Foundation.NSNotification notification)
+		public override void WillResignActive (NSNotification notification)
 		{
 			IntPtr focusWindow = driver.GetFocus ();
 			if (focusWindow != IntPtr.Zero)
@@ -29,7 +32,7 @@ namespace System.Windows.Forms.CocoaInternal
 
 			if (XplatUICocoa.Grab.Hwnd != IntPtr.Zero) {
 				driver.SendMessage (Hwnd.ObjectFromHandle (XplatUICocoa.Grab.Hwnd).Handle, 
-					Msg.WM_LBUTTONDOWN, (IntPtr)MsgButtons.MK_LBUTTON, 
+					Msg.WM_LBUTTONUP, (IntPtr)MsgButtons.MK_LBUTTON, 
 					(IntPtr) (driver.MousePosition.X << 16 | driver.MousePosition.Y));
 			}
 
@@ -37,7 +40,32 @@ namespace System.Windows.Forms.CocoaInternal
 				if (utility_window.IsVisible)
 					utility_window.OrderOut (utility_window);
 
-			driver.PostMessage (XplatUI.GetActive (), Msg.WM_ACTIVATEAPP, (IntPtr)WindowActiveFlags.WA_INACTIVE, IntPtr.Zero);
+			driver.SendMessage (XplatUI.GetActive (), Msg.WM_ACTIVATEAPP, (IntPtr)WindowActiveFlags.WA_INACTIVE, IntPtr.Zero);
+		}
+
+		void CreateMenu()
+		{
+			NSMenu mainMenu = new NSMenu();
+			NSMenu appMenu = new NSMenu(Application.ProductName);
+			NSMenuItem quitItem = appMenu.AddItem("Quit " + Application.ProductName, new Selector("terminate:"), "q");
+			quitItem.KeyEquivalentModifierMask = NSEventModifierMask.CommandKeyMask;// | NSEventModifierMask.AlternateKeyMask;
+			NSMenuItem appItem = new NSMenuItem();
+			appItem.Title = Application.ProductName;
+			appItem.Submenu = appMenu;
+			mainMenu.AddItem(appItem);
+
+			NSMenu windowMenu = new NSMenu();
+			windowMenu.Title = "Window";
+			windowMenu.AddItem("Minimize", new Selector("performMiniaturize:"), "");
+			windowMenu.AddItem("Zoom", new Selector("performZoom:"), "");
+			windowMenu.AddItem(NSMenuItem.SeparatorItem);
+			windowMenu.AddItem("Bring All to Front", new Selector("arrangeInFront:"), "");
+			NSMenuItem windowItem = new NSMenuItem();
+			windowItem.Submenu = windowMenu;
+			mainMenu.AddItem(windowItem);
+
+			NSApplication.SharedApplication.MainMenu = mainMenu;
+			NSApplication.SharedApplication.WindowsMenu = windowMenu;
 		}
 	}
 }
