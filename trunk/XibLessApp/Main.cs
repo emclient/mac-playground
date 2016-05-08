f8bd8bcbea76367f8ee832d5cbd4d186bac445f5
Jiri Volejnik <volejnik@emclient.com>
2016-05-08 20:59:09 +0000

    Added XibLessApp project - a minimalistic xamarin app without nib for testing menu issues in modal and regular loop.

---
+++ trunk/XibLessApp/Main.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace XibLessApp
+{
+	class AppDelegate : NSApplicationDelegate
+	{
+	}
+
+	class Program
+	{
+		static NSApplication app;
+
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+
+			app = NSApplication.SharedApplication;
+			app.Delegate = new AppDelegate();
+			app.ActivationPolicy = NSApplicationActivationPolicy.Regular;
+			app.FinishLaunching();
+			app.Menu = Program.CreateMenu();
+
+			//app.Run()
+
+			var dialog = Program.WindowWithTitle("Modal");
+			Program.RunModalWindow(dialog);
+
+			var window = Program.WindowWithTitle("Normal");
+			window.MakeKeyAndOrderFront(app);
+			Program.Run();
+		}
+
+		static void Run()
+		{
+			while (true)
+			{
+				var mask = NSEventMask.AnyEvent;
+				var @event = NSApplication.SharedApplication.NextEvent(mask, NSDate.DistantFuture, NSRunLoop.NSDefaultRunLoopMode, true);
+
+				if (@event == null)
+					break;
+
+				app.SendEvent(@event);
+			}
+		}
+
+		static void RunModalWindow(NSWindow window)
+		{
+			var session = app.BeginModalSession(window);
+			while (true)
+			{
+				var mask = NSEventMask.AnyEvent;
+				var @event = app.NextEvent(mask, NSDate.DistantFuture, NSRunLoop.NSDefaultRunLoopMode, true);
+
+				if (@event != null)
+				{
+					app.SendEvent(@event);
+
+					if (!window.IsVisible)
+						break;
+				}
+			}
+			app.EndModalSession(session);
+		}
+
+		static NSWindow WindowWithTitle(String title)
+		{
+			var style = NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Resizable;
+			var window = new NSWindow(new RectangleF(0, 0, 480, 320), style, NSBackingStore.Buffered, false);
+			window.Title = title;
+			return window;
+		}
+
+		static NSMenu CreateMenu()
+		{
+			var root = new NSMenu("MainMenu");
+
+			var appMenu = new NSMenu("AppMenu");
+			//appMenu.AddItem(new NSMenuItem("Hide", "h", (sender, e) => { Console.WriteLine("Hide"); }));
+			//appMenu.AddItem(new NSMenuItem("Hide others", "o", (sender, e) => { Console.WriteLine("Hide Others"); }));
+			//appMenu.AddItem(new NSMenuItem("Quit", "q", (sender, e) => { Console.WriteLine("Quit"); app.Terminate(app); }));
+			appMenu.AddItem(new NSMenuItem("Hide", new Selector("hide:"), "h"));
+			appMenu.AddItem(new NSMenuItem("Hide Others", new Selector("hideOtherApplications:"), "o"));
+			appMenu.AddItem(new NSMenuItem("Quit XibLessApp", new Selector("terminate:"), "q"));
+
+			var appMenuItem = root.AddItem("AppMenuItem", null, "");
+			root.SetSubmenu(appMenu, appMenuItem);
+
+			return root;
+		}
+	}
+}
+
