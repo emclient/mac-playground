b61602f605591c3f26349bd426af1600cf89739d
Jiri Volejnik <volejnik@emclient.com>
2016-05-10 23:04:10 +0000

    Empty SetWrapMode method added to ImageAttributes to fix mac build after changes in formSettings.

--- trunk/sysdrawing-coregraphics/System.Drawing.Imaging/ImageAttributes.cs
+++ trunk/sysdrawing-coregraphics/System.Drawing.Imaging/ImageAttributes.cs
@@ -96,6 +96,21 @@ namespace System.Drawing.Imaging
 			isGammaSet = true;
 		}
 
+		public void SetWrapMode(WrapMode mode)
+		{
+			SetWrapMode(mode, new Color(), false);
+		}
+
+		public void SetWrapMode(WrapMode mode, Color color)
+		{
+			SetWrapMode(mode, color, false);
+		}
+
+		public void SetWrapMode(WrapMode mode, Color color, bool clamp)
+		{
+			// FIXME
+		}
+
 		#region ICloneable implementation
 		public object Clone ()
 		{
