1b1fc1f727ce8336f16703ea68ceb7459519a041
Jiri Volejnik <volejnik@emclient.com>
2016-05-19 14:53:21 +0000

    Mac: Added copying libhunspell.dylib into Contents folder when packaging the native Mac application. This had not been necessary until the last mono build.

--- trunk/Experiments/eM Client 32/copy_app.sh
+++ trunk/Experiments/eM Client 32/copy_app.sh
@@ -4,6 +4,8 @@ export OUT_DIR="$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
 mkdir -p "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
 cp -r "$BIN_DIR/"* "$OUT_DIR"
 
+cp "$BIN_DIR/"libhunspell.dylib "$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH"
+
 #export BIN_DIR="$PROJECT_DIR/../ConsoleTest/bin/Debug"
 #export TMP_DIR="$PROJECT_DIR/../../../bin/x86/tmp"
 #export OUT_DIR="$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/lib/emclient"
