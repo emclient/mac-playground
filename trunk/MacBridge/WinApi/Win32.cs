1558c39b2d476a0793384f47067fcf50f6d8f614
Jiri Volejnik <aconcagua21@volny.cz>
2016-05-26 14:01:06 +0000

    Win32 code from BaseDataObject.cs added to MacBridge/WinApi project (mac version)

--- trunk/MacBridge/WinApi/Win32.cs
+++ trunk/MacBridge/WinApi/Win32.cs
@@ -10,5 +10,15 @@ namespace WinApi
 		{
 			Debug.WriteLine("Not Implemented: " + method.ReflectedType.Name + "." + method.Name + (details == null ? String.Empty : " (" + details.ToString() + ")"));
 		}
+
+		/// <summary>
+		/// Returns true iff the HRESULT is a success code.
+		/// </summary>
+		/// <param name="hr">HRESULT to check.</param>
+		/// <returns>True iff a success code.</returns>
+		public static bool SUCCEEDED(int hr)
+		{
+			return (0 <= hr);
+		}
 	}
 }
