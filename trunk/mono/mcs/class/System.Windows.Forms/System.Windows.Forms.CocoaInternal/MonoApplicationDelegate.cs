9f92420b12871bb406021374518a79307aae1fb5
Jiri Volejnik <aconcagua21@volny.cz>
2016-06-13 12:48:07 +0000

    Mac: Code cleanup

--- trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/MonoApplicationDelegate.cs
+++ trunk/mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/MonoApplicationDelegate.cs
@@ -85,31 +85,6 @@ namespace System.Windows.Forms.CocoaInternal
 			driver.SendMessage (XplatUI.GetActive (), Msg.WM_ACTIVATEAPP, (IntPtr)WindowActiveFlags.WA_INACTIVE, IntPtr.Zero);
 		}
 
-		void CreateMenu()
-		{
-			NSMenu mainMenu = new NSMenu();
-			NSMenu appMenu = new NSMenu(Application.ProductName);
-			NSMenuItem quitItem = appMenu.AddItem("Quit " + Application.ProductName, new Selector("terminate:"), "q");
-			quitItem.KeyEquivalentModifierMask = NSEventModifierMask.CommandKeyMask;// | NSEventModifierMask.AlternateKeyMask;
-			NSMenuItem appItem = new NSMenuItem();
-			appItem.Title = Application.ProductName;
-			appItem.Submenu = appMenu;
-			mainMenu.AddItem(appItem);
-
-			NSMenu windowMenu = new NSMenu();
-			windowMenu.Title = "Window";
-			windowMenu.AddItem("Minimize", new Selector("performMiniaturize:"), "");
-			windowMenu.AddItem("Zoom", new Selector("performZoom:"), "");
-			windowMenu.AddItem(NSMenuItem.SeparatorItem);
-			windowMenu.AddItem("Bring All to Front", new Selector("arrangeInFront:"), "");
-			NSMenuItem windowItem = new NSMenuItem();
-			windowItem.Submenu = windowMenu;
-			mainMenu.AddItem(windowItem);
-
-			NSApplication.SharedApplication.MainMenu = mainMenu;
-			NSApplication.SharedApplication.WindowsMenu = windowMenu;
-		}
-
 		public static List<Form> GetOpenForms()
 		{
 			var forms = new List<Form>(Application.OpenForms.Count);
