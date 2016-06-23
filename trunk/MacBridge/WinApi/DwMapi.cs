a6baf0a4a948ce2c2270cc8501f7b9bb5248a77f
Jiri Volejnik <aconcagua21@volny.cz>
2016-06-23 21:32:24 +0000

    Mac: Fixing build (adding DwmApi decl)

---
+++ trunk/MacBridge/WinApi/DwMapi.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace WinApi
+{
+	public static partial class Win32
+	{
+		public static void DwmGetColorizationParameters(ref DWMCOLORIZATIONPARAMS parms)
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+		}
+	}
+}
