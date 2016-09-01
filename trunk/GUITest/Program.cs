c0c10829da79f6edd8bc8e718b17a60c7b785ec5
Jiri Volejnik <volejnik@emclient.com>
2016-09-01 14:24:29 +0000

    Missing files added to svn

---
+++ trunk/GUITest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GUITest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var suite = new MainTestSuite().Initialize();
+
+			FormsTest.Program.Main(args);
+
+			suite.Done();
+		}
+	}
+}
