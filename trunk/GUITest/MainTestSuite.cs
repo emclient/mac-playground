2734b3d7e2d09a9e1e78778750b4770c21ccee49
Jiri Volejnik <volejnik@emclient.com>
2016-09-03 21:59:08 +0000

    GUITest reafactored. NUnit used.

--- trunk/GUITest/MainTestSuite.cs
+++ trunk/GUITest/MainTestSuite.cs
@@ -1,243 +1,54 @@
 ﻿using System;
-using FormsTest;
 using System.Windows.Forms;
 using System.Threading;
-using System.ComponentModel;
-using System.Reflection;
-using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace GUITest
 {
-	internal class MainTestSuite
+    [TestFixture]
+    public class MainTestSuite
 	{
-		#region Fields
-
-		BackgroundWorker worker = new BackgroundWorker();
-		List<Action> succeded = new List<Action>();
-		List<Action> failed = new List<Action>();
-
-		MainForm mainForm;
-
-		#endregion //Fields
-
-		#region Public API
-
-		public MainTestSuite Initialize()
+		[Test]
+        public void BrowserTest()
 		{
-			worker.DoWork += Run;
-			worker.RunWorkerCompleted += Completed;
-			worker.RunWorkerAsync();
+			var button = (Button)UI.GetMember("MainForm.button2", typeof(Button));
+            UI.Perform(() => button.PerformClick());
 
-			return this;
-		}
-
-		public void Done()
-		{
-		}
-
-		#endregion //Public API
-
-		#region Tests
-
-		void DropDownTest()
-		{
-		}
-
-		void BrowserTest()
-		{
-			var button = (Button)GetMember("MainForm.button2", typeof(Button));
-            mainForm.UIThread(() =>	button.PerformClick());
-
-			var webForm = WaitForForm("WebForm");
+			var webForm = UI.WaitForForm("WebForm");
 			ThrowIfNull(webForm, "WebForm not found");
 
             Thread.Sleep(1000);
-            mainForm.UIThread(() =>	webForm.Close());
+            UI.Perform(() => webForm.Close());
 		}
 
-		void DialogTest()
+        [Test]
+        public void DialogTest()
 		{
-			var button = (Button)GetMember("MainForm.button3", typeof(Button));
-            Delay(1.0, () => { SendKeys.SendWait("{ESC}");  });
-            mainForm.UIThread(() => button.PerformClick());
+			var button = (Button)UI.GetMember("MainForm.button3", typeof(Button));
+            UI.Delay(1.0, () => { SendKeys.SendWait("{ESC}");  });
+            UI.Perform(() => button.PerformClick());
 		}
 
-        void OpenFileDialogTest()
+        [Test]
+        public void OpenFileDialogTest()
         {
-            var combo = (ComboBox)GetMember("MainForm.dialogTypeCombo", typeof(ComboBox));
-            mainForm.UIThread(() => combo.Focus());
+            var combo = (ComboBox)UI.GetMember("MainForm.dialogTypeCombo", typeof(ComboBox));
+            UI.Perform(() => combo.Focus());
             SendKeys.SendWait("{F4}");
             SendKeys.SendWait("{DOWN}");
             SendKeys.SendWait("{ENTER}");
             Thread.Sleep(500);
 
-            var button = (Button)GetMember("MainForm.button3", typeof(Button));
-            Delay(1.5, () => { SendKeys.SendWait("{ESC}"); });
-            mainForm.UIThread(() => button.PerformClick());
+            var button = (Button)UI.GetMember("MainForm.button3", typeof(Button));
+            UI.Delay(1.5, () => { SendKeys.SendWait("{ESC}"); });
+            UI.Perform(() => button.PerformClick());
             Thread.Sleep(2000);
         }
         
-        #endregion //Tests
-
-		#region Internals
-
-		void Run(object sender, EventArgs e)
-		{
-			Console.WriteLine("Starting GUI tests..");
-			Console.WriteLine("--------------------");
-
-			mainForm = WaitForForm(typeof(MainForm)) as MainForm;
-
-			Execute(BrowserTest);
-			Execute(DialogTest);
-            Execute(OpenFileDialogTest);
-		}
-
-		void Completed(object sender, EventArgs e)
-		{
-			Console.WriteLine("GUI tests finished.");
-			Console.WriteLine("--------------------");
-            Console.WriteLine("Succeeded: {0}", succeded.Count);
-			Console.WriteLine("Failed:    {0}", failed.Count);
-			Console.WriteLine("--------------------");
-
-			if (mainForm != null)
-                mainForm.UIThread(() => { mainForm.Close(); });
-		}
-
-		void Execute(Action action)
-		{
-			try
-			{
-				action();
-
-				succeded.Add(action);
-				Console.WriteLine("{0}: Success!", action.Method.Name);
-			}
-			catch (Exception e)
-			{
-				failed.Add(action);
-				Console.WriteLine("{0}: Failure! Details: '{1}'", action.Method.Name, e);
-			}
-		}
-
-		Form WaitForForm(string nameOrType, double timeout = 3.0, double interval = 0.1)
-		{
-			var due = DateTime.Now.AddSeconds(timeout);
-			while (DateTime.Now.CompareTo(due) < 0)
-			{
-				var form = FindForm(nameOrType);
-				if (form != null)
-					return form;
-				Thread.Sleep((int)(1000 * interval));
-			}
-			return null;
-		}
-
-		Form WaitForForm(Type type, double timeout = 3.0, double interval = 0.1)
-		{
-			var due = DateTime.Now.AddSeconds(timeout);
-			while (DateTime.Now.CompareTo(due) < 0)
-			{
-				var form = FindForm(type);
-				if (form != null)
-					return form;
-				Thread.Sleep((int)(1000 * interval));
-			}
-			return null;
-		}
-
-		object GetMember(string path, Type type, int maxLevel = int.MaxValue)
-		{
-			var parts = path.Split('.');
-
-			if (parts.Length == 0)
-				return null;
-
-			var form = FindForm(parts[0]);
-			ThrowIfNull(form, "Form not found: " + type.Name);
-
-			if (parts.Length == 1)
-				return form;
-
-			object parent = form;
-			object member = null;
-			for (int i = 1; i < parts.Length; ++i)
-			{
-				member = FindMember(parent, parts[i]);
-				if (member == null)
-					return null;
-
-				parent = member;
-			}
-
-			ThrowIfNull(member, String.Format("Member {0} not found", path));
-			return member;
-		}
-
 		void ThrowIfNull(object instance, string message = "Instance not found")
 		{
 			if (instance == null)
 				throw new ApplicationException(message);
 		}
-
-		Form FindForm(Type type)
-		{
-			foreach (Form form in Application.OpenForms)
-				if (form.GetType().Equals(type))
-					return form;
-			return null;
-		}
-
-		Form FindForm(string nameOrType)
-		{
-			foreach (Form form in Application.OpenForms)
-				if (nameOrType.Equals(form.Name) || nameOrType.Equals(form.Text))
-					return form;
-
-			foreach (Form form in Application.OpenForms)
-			{
-				var typeName = form.GetType().Name;
-				if (typeName.Equals(nameOrType) || typeName.EndsWith("." + nameOrType))
-					return form;
-			}
-
-			return null;
-		}
-
-		public static object FindMember(object instance, string name, Type type = null, int maxLevel = 1)
-		{
-			var field = FindField(instance, name, maxLevel);
-			if (field != null)
-			{
-				var value = field.GetValue(instance);
-				if (value != null || value.GetType().Equals(type))
-					return value;
-			}
-			return null;
-		}
-
-		public static FieldInfo FindField(object instance, string name, int maxLevel = 1)
-		{
-			if (instance == null)
-				return null;
-
-			var type = instance.GetType();
-			var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-
-			foreach (var field in fields)
-				if (field.Name.Equals(name))
-					return field;
-
-			return null;
-		}
-
-		void Delay(double interval, Action action)
-		{
-			System.Threading.Timer timer = null;
-            timer = new System.Threading.Timer((e) => { action(); });
-			timer.Change((int)(1000 * interval), int.MaxValue);
-		}
-		#endregion // internals
 	}
 }
---
+++ trunk/GUITest/UI.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GUITest
+{
+    public static class UI
+    {
+        static Form main;
+
+        public static void Initialize(Type mainFormType)
+        {
+            main = WaitForForm(mainFormType);
+        }
+
+        public static void Initialize(string mainFormNameOrType)
+        {
+            main = WaitForForm(mainFormNameOrType);
+        }
+
+        public static Form WaitForForm(string nameOrType, double timeout = 3.0, double interval = 0.1)
+        {
+            var due = DateTime.Now.AddSeconds(timeout);
+            while (DateTime.Now.CompareTo(due) < 0)
+            {
+                var form = FindForm(nameOrType);
+                if (form != null)
+                    return form;
+                Thread.Sleep((int)(1000 * interval));
+            }
+            return null;
+        }
+
+        public static Form WaitForForm(Type type, double timeout = 3.0, double interval = 0.1)
+        {
+            var due = DateTime.Now.AddSeconds(timeout);
+            while (DateTime.Now.CompareTo(due) < 0)
+            {
+                var form = FindForm(type);
+                if (form != null)
+                    return form;
+                Thread.Sleep((int)(1000 * interval));
+            }
+            return null;
+        }
+
+        public static object GetMember(string path, Type type, int maxLevel = int.MaxValue)
+        {
+            var parts = path.Split('.');
+
+            if (parts.Length == 0)
+                return null;
+
+            var form = FindForm(parts[0]);
+            ThrowIfNull(form, "Form not found: " + type.Name);
+
+            if (parts.Length == 1)
+                return form;
+
+            object parent = form;
+            object member = null;
+            for (int i = 1; i < parts.Length; ++i)
+            {
+                member = FindMember(parent, parts[i]);
+                if (member == null)
+                    return null;
+
+                parent = member;
+            }
+
+            ThrowIfNull(member, String.Format("Member {0} not found", path));
+            return member;
+        }
+
+        internal static void Close()
+        {
+            Perform(() => { main.Close(); });
+        }
+
+        public static Form FindForm(Type type)
+        {
+            foreach (Form form in Application.OpenForms)
+                if (form.GetType().Equals(type))
+                    return form;
+            return null;
+        }
+
+        public static Form FindForm(string nameOrType)
+        {
+            foreach (Form form in Application.OpenForms)
+                if (nameOrType.Equals(form.Name) || nameOrType.Equals(form.Text))
+                    return form;
+
+            foreach (Form form in Application.OpenForms)
+            {
+                var typeName = form.GetType().Name;
+                if (typeName.Equals(nameOrType) || typeName.EndsWith("." + nameOrType))
+                    return form;
+            }
+
+            return null;
+        }
+
+        public static object FindMember(object instance, string name, Type type = null, int maxLevel = 1)
+        {
+            var field = FindField(instance, name, maxLevel);
+            if (field != null)
+            {
+                var value = field.GetValue(instance);
+                if (value != null || value.GetType().Equals(type))
+                    return value;
+            }
+            return null;
+        }
+
+        public static FieldInfo FindField(object instance, string name, int maxLevel = 1)
+        {
+            if (instance == null)
+                return null;
+
+            var type = instance.GetType();
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+
+            foreach (var field in fields)
+                if (field.Name.Equals(name))
+                    return field;
+
+            return null;
+        }
+
+        public static void Delay(double interval, Action action)
+        {
+            System.Threading.Timer timer = null;
+            timer = new System.Threading.Timer((e) => { action(); });
+            timer.Change((int)(1000 * interval), int.MaxValue);
+        }
+
+        // Use this method to execute your action in the UI thread.
+        public static void Perform(Action action)
+        {
+            ThrowIfNull(main, "UI not initialized - root control is null.");
+            main.UIThread(action);
+        }
+
+        public static void ThrowIfNull(object instance, string message = "Instance not found")
+        {
+            if (instance == null)
+                throw new ApplicationException(message);
+        }
+    }
+}
