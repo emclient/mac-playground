16b11120bf22b4eade0b99cd28d102cf06c8b55f
Jiri Volejnik <aconcagua21@volny.cz>
2016-10-26 16:18:16 +0000

    Mac: Fixing Xamarin build

--- sysdrawing-coregraphics/System.Drawing/Graphics-DrawString.cs
+++ sysdrawing-coregraphics/System.Drawing/Graphics-DrawString.cs
@@ -250,8 +250,8 @@ namespace System.Drawing
 				//var verticalOffset = 0.0f;
 				while (start < length)
 				{
-					var count = noWrap ? length : typesetter.SuggestLineBreak (start, insetBounds.Height);
-					double lineHeight = NMath.Ceiling(1 + GetLineHeight(typesetter, start, count)); // +1 matches best to CTFramesetter's behavior
+					var count = noWrap ? length : (int)typesetter.SuggestLineBreak (start, insetBounds.Height);
+					var lineHeight = NMath.Ceiling(1 + GetLineHeight(typesetter, start, count)); // +1 matches best to CTFramesetter's behavior
 					//if (format.LineAlignment == StringAlignment.Far && baselineOffset + lineHeight > layoutRectangle.Height)
 					//	break;
 					baselineOffset += (float)lineHeight;
@@ -275,8 +275,8 @@ namespace System.Drawing
 				{
 					double y = insetBounds.Y + .5f;
 					while (start < length && y < insetBounds.Bottom) {
-						var count = noWrap ? length : typesetter.SuggestLineBreak (start, insetBounds.Width);
-						var lineHeight = Math.Ceiling(1 + GetLineHeight(typesetter, start, count));
+						var count = noWrap ? length : (int)typesetter.SuggestLineBreak (start, insetBounds.Width);
+						var lineHeight = NMath.Ceiling(1 + GetLineHeight(typesetter, start, count));
 						if (format.LineAlignment == StringAlignment.Far && baselineOffset + lineHeight > layoutRectangle.Height)
 							break;
 						baselineOffset += (float)lineHeight;
@@ -304,8 +304,8 @@ namespace System.Drawing
 
 				// Create and initialize some values from the bounds.
 				nfloat ascent, descent, leading;
-				double lineWidth = line.GetTypographicBounds(out ascent, out descent, out leading);
-				double lineHeight = NMath.Ceiling(ascent + descent + leading + 1); // +1 matches best to CTFramesetter's behavior  
+				var lineWidth = line.GetTypographicBounds(out ascent, out descent, out leading);
+				var lineHeight = NMath.Ceiling(ascent + descent + leading + 1); // +1 matches best to CTFramesetter's behavior  
 
 				// Calculate the string format if need be
 				var penFlushness = 0.0f;
@@ -479,13 +479,13 @@ namespace System.Drawing
 			return atts;
 		}
 
-		internal static double GetLineHeight(CTTypesetter typesetter, int start, int count)
+		internal static nfloat GetLineHeight(CTTypesetter typesetter, int start, int count)
 		{
 			using (var line = typesetter.GetLine(new NSRange(start, count)))
 				return GetLineHeight(line);
 		}
 
-		internal static double GetLineHeight(CTLine line)
+		internal static nfloat GetLineHeight(CTLine line)
 		{
 			nfloat ascent, descent, leading;
 			line.GetTypographicBounds(out ascent, out descent, out leading);
