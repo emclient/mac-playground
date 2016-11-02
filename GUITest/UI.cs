d64eeba20d9002c6cc4ec40a38132a01ed2896bc
Tomas Svoboda <svoboda@emclient.com>
2016-11-02 10:10:33 +0000

    Move Mouse class to separate file Move control and form handlers to a new folder structure Add ThrowIfNotVisible delegate to Click methods

--- GUITest/UI.cs
+++ GUITest/UI.cs
@@ -380,34 +380,6 @@ namespace GUITest
                 throw new ApplicationException(message);
         }
 
-		public static void ThrowIfNotVisible(object control, string message = "Control is not visible")
-		{
-			if (control is Control)
-			{
-				if (!((Control)control).Visible)
-					throw new ApplicationException(message);
-			}
-			else if (control is ControlDataGridWithSourceIndex)
-			{
-				if (!((ControlDataGridWithSourceIndex)control).DataGrid.Visible)
-					throw new ApplicationException(message);
-			}
-			else if (control is ControlPanelTabSwitcherWithTabIndex)
-			{
-				if (!((ControlPanelTabSwitcherWithTabIndex)control).PanelTabSwitcher.Visible)
-					throw new ApplicationException(message);
-			}
-			else if (control is ToolStripItem)
-			{
-				if (!((ToolStripItem)control).Visible)
-					throw new ApplicationException(message);
-			}
-			else
-			{
-				throw new ApplicationException("Control was not cast as any of the known types");
-			}
-		}
-
 		public static void Type(char c, double delay = 0.05)
         {
             Key(c);
@@ -425,170 +397,14 @@ namespace GUITest
             Win32.SendInput(1, new INPUT[] { input }, INPUT.Size);
         }
 
-        private static void Sleep(double interval)
+        internal static void Sleep(double interval)
         {
             Thread.Sleep((int)(1000 * interval));
         }
 
 		public static class Mouse
 		{
-			enum ClickType
-			{
-				Left,
-				Right,
-				DoubleClick
-			}
-
-			public static void Click(Control control)
-			{
-				Click(ClickType.Left, GetCenterOfControl, control);
-			}
-
-			public static void RightClick(Control control)
-			{
-				Click(ClickType.Right, GetCenterOfControl, control);
-			}
-
-			public static void DoubleClick(Control control)
-			{
-				Click(ClickType.DoubleClick, GetCenterOfControl, control);
-			}
-
-			public static void Click(ControlDataGrid control, int row)
-			{
-				Click(ClickType.Left, GetCenterOfControlDataGridRow, new ControlDataGridWithSourceIndex(control, row));
-			}
-
-			public static void RightClick(ControlDataGrid control, int row)
-			{
-				Click(ClickType.Right, GetCenterOfControlDataGridRow, new ControlDataGridWithSourceIndex(control, row));
-			}
-
-			public static void DoubleClick(ControlDataGrid control, int row)
-			{
-				Click(ClickType.DoubleClick, GetCenterOfControlDataGridRow, new ControlDataGridWithSourceIndex(control, row));
-			}
-
-			public static void Click(ToolStripMenuItem control)
-			{
-				Click(ClickType.Left, GetCenterOfToolStripMenuItem, control);
-			}
-
-			public static void Click(ControlPanelTabSwitcherWithTabIndex control)
-			{
-				Click(ClickType.Left, GetCenterOfControlPanelTabSwitcherTab, control);
-			}
-
-			private static void Click<T>(ClickType clickType, GetScreenRectangleOfControl<T> getMouseDestinationDelegate, T destinationObject)
-			{
-				ThrowIfNotVisible(destinationObject);
-				MOUSEEVENTF downFlags = (clickType == ClickType.Right) ? MOUSEEVENTF.RIGHTDOWN : MOUSEEVENTF.LEFTDOWN;
-				MOUSEEVENTF upFlags = (clickType == ClickType.Right) ? MOUSEEVENTF.RIGHTUP : MOUSEEVENTF.LEFTUP;
-				int numberOfClicks = (clickType == ClickType.DoubleClick) ? 2 : 1;
-				for (int i = 0; i < numberOfClicks; i++)
-				{
-					MoveTo(getMouseDestinationDelegate, destinationObject);
-					SendInput(downFlags);
-					Thread.Sleep(SystemInformation.DoubleClickTime / 8);
-					MoveTo(getMouseDestinationDelegate, destinationObject);
-					SendInput(upFlags);
-					Thread.Sleep(SystemInformation.DoubleClickTime / 8);
-
-					if (clickType == ClickType.DoubleClick)
-						Thread.Sleep(SystemInformation.DoubleClickTime / 4);
-				}
-
-			}
-
-			private static void SendInput(MOUSEEVENTF flags)
-			{
-				var input = new INPUT { type = InputType.MOUSE };
-				input.U.mi.dx = input.U.mi.dy = input.U.mi.mouseData = 0;
-				input.U.mi.dwFlags = flags;
-				Win32.SendInput(1, new INPUT[] { input }, INPUT.Size);
-			}
-
-			private static Point GetCenterOfRectangle(Rectangle rectangle)
-			{
-				return new Point((rectangle.Left + rectangle.Right) / 2, (rectangle.Top + rectangle.Bottom) / 2);
-			}
-
-			private static Rectangle GetCenterOfControl(Control control)
-			{
-				return control.RectangleToScreen(control.ClientRectangle);
-			}
-
-			private static Rectangle GetCenterOfControlDataGridRow(ControlDataGridWithSourceIndex control)
-			{
-				Rectangle relativeRowRectangle = control.DataGrid.GetRowBoundaries(control.DataGrid.GetRowFromDataSourceIndex(control.Row, true));
-				return control.DataGrid.RectangleToScreen(relativeRowRectangle);
-			}
-
-			private static Rectangle GetCenterOfControlPanelTabSwitcherTab(ControlPanelTabSwitcherWithTabIndex control)
-			{
-				Rectangle relativeTabRectangle = control.PanelTabSwitcher.GetTabRectangleForPanel(control.PanelTabSwitcher.Controls[control.TabIndex] as MailClient.Common.UI.Controls.ControlPanelSwitcher.SwitchPanel);
-				return control.PanelTabSwitcher.RectangleToScreen(relativeTabRectangle);
-			}
-
-			private static Rectangle GetCenterOfToolStripMenuItem(ToolStripMenuItem control)
-			{
-				return control.Owner.RectangleToScreen(control.Bounds);
-			}
-
-			private delegate Rectangle GetScreenRectangleOfControl<T>(T destinationObject);
-
-			private static void MoveTo<T>(GetScreenRectangleOfControl<T> getScreenRectangleOfControl, T destinationObject)
-			{
-				var dt = 0.02;
-				var step = new Point(0, 0);
-
-				while (true)
-				{
-					Point center = GetCenterOfRectangle(Perform(() => { return getScreenRectangleOfControl(destinationObject); }));
-					Rectangle rectangle = new Rectangle(center.X - 1, center.Y - 1, 2, 2);
-
-					Point cursorPosition;
-					Win32.GetCursorPos(out cursorPosition);
-					if (rectangle.Contains(cursorPosition))
-						break;
-
-					Point coordinatesDifference = new Point(center.X - cursorPosition.X, center.Y - cursorPosition.Y);
-					double distance = Math.Sqrt(coordinatesDifference.X * coordinatesDifference.X + coordinatesDifference.Y * coordinatesDifference.Y);
-
-					if (distance > 1)
-					{
-						double speed = SpeedFromDistance(distance); // Faster when far away, slower when approaching the destination.
-						double pixelsPerInterval = speed * dt;
-						step.X = (int)(pixelsPerInterval * (coordinatesDifference.X / distance));
-						step.Y = (int)(pixelsPerInterval * (coordinatesDifference.Y / distance));
-					}
-
-					if (step.IsEmpty)
-						break;
-
-					var input = new INPUT { type = InputType.MOUSE };
-					input.U.mi.dx = step.X;
-					input.U.mi.dy = step.Y;
-					input.U.mi.dwFlags = MOUSEEVENTF.MOVE;
-					input.U.mi.mouseData = 0;
-
-					Win32.SendInput(1, new INPUT[] { input }, INPUT.Size);
-					Sleep(dt);
-				}
-
-				Sleep(dt);
-			}
-
-			private static readonly double[,] speeds = { { 2000, 4000 }, { 1000, 3000 }, { 100, 2000 }, { 10, 500 }, { 4, 100 } };
-
-			private static double SpeedFromDistance(double distance)
-			{
-				// TODO: Make it independent on the DPI, so that it works the same way on every monitor.
-				for (int i = 0; i < speeds.GetLength(0); i++)
-					if (distance > speeds[i, 0])
-						return speeds[i, 1];
-				return speeds[speeds.GetLength(0) - 1, 1];
-			}
+			
 		}
 	}
 }
