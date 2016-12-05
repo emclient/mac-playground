7c3be8c4f863df857f8e578eb8db0012346bb6a6
Jiri Volejnik <volejnik@emclient.com>
2016-12-05 11:10:03 +0000

    Mac: TextBoxForm added to OSXPlayground for debugging TextBox issues.

---
+++ FormsTest/TextBoxForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailClient.Common.UI;
+
+namespace FormsTest
+{
+    public partial class TextBoxForm : Form
+    {
+        public TextBoxForm()
+        {
+            InitializeComponent();
+
+            // Mac layout debugging
+            //this.DebugAllControls();
+        }
+    }
+}
