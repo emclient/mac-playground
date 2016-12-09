4c6d5562057ed353343d1e827c5fc5f7ec47d78c
Jiri Volejnik <volejnik@emclient.com>
2016-12-09 12:18:40 +0000

    Mac: Working on FlowLayoutPanel issues.

--- FormsTest/LayoutIssues/ImapOptions/ImapOptionsForm.cs
+++ FormsTest/LayoutIssues/ImapOptions/ImapOptionsForm.cs
@@ -5,9 +5,29 @@ namespace FormsTest
 {
 	public partial class ImapOptionsForm : Form
 	{
+		private MailClient.Protocols.Imap.ImapOptionsControl imapOptionsControl;
+
 		public ImapOptionsForm()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			if (imapOptionsControl == null)
+			{
+				panel1.Hide();
+				//panel1.SuspendLayout();
+
+				imapOptionsControl = new MailClient.Protocols.Imap.ImapOptionsControl();
+				//imapOptionsControl.Size = panel1.ClientSize;
+				this.Controls.Add(imapOptionsControl);
+				imapOptionsControl.Dock = DockStyle.Fill;
+
+				//panel1.ResumeLayout();
+			}
 		}
 	}
 }
