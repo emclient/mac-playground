6d176f75b2739ca5425234561ebc15b9f3737257
Filip Navara <navara@emclient.com>
2017-03-17 14:43:22 +0000

    Make the custom ResGen less intrusive.

--- ResGen/monoresgen.cs
+++ ResGen/monoresgen.cs
@@ -36,7 +36,8 @@ class ResGen {
 			//swf = Assembly.Load ("System.Windows.Forms-net_4_5.dll");
 			//swf.GetType("System.Windows.Forms.Application").GetMethod("DoEvents", BindingFlags.Static | BindingFlags.Public).Invoke(null, null);
 			swf = typeof(System.Windows.Forms.Control).Assembly;
-			System.Windows.Forms.Application.DoEvents();
+			//System.Windows.Forms.Application.DoEvents();
+			(Type.GetType("AppKit.NSApplication, Xamarin.Mac") ?? Type.GetType("MonoMac.AppKit.NSApplication, MonoMac")).GetMethod("Init", BindingFlags.Static | BindingFlags.Public).Invoke(null, null);
 			resxr = swf.GetType ("System.Resources.ResXResourceReader");
 			resxw = swf.GetType ("System.Resources.ResXResourceWriter");
 		} catch (Exception e) {
