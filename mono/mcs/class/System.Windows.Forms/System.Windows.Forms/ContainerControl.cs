f6437a46246cdb95263a44c51206af966571bb5d
Filip Navara <navara@emclient.com>
2017-04-10 20:10:07 +0000

    Move layout code for GetPreferredSize to relavant LayoutEngine implementations. This makes it possible to share the layout code for ContainerControl, UserControl and regular Panel and fixes the previously broken implementations in Panel and ContainerControl.

--- mono/mcs/class/System.Windows.Forms/System.Windows.Forms/ContainerControl.cs
+++ mono/mcs/class/System.Windows.Forms/System.Windows.Forms/ContainerControl.cs
@@ -1,4 +1,4 @@
-// Permission is hereby granted, free of charge, to any person obtaining
+﻿// Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
 // without limitation the rights to use, copy, modify, merge, publish,
@@ -823,7 +823,16 @@ namespace System.Windows.Forms {
 		{
 			base.OnLayout (e);
 		}
-		
+
+		internal override Size GetPreferredSizeCore (Size proposedSize)
+		{
+			// Translating 0,0 from ClientSize to actual Size tells us how much space
+			// is required for the borders.
+			Size borderSize = SizeFromClientSize(Size.Empty);
+			Size totalPadding = borderSize + Padding.Size;
+			return LayoutEngine.GetPreferredSize(this, proposedSize) + totalPadding;
+		}
+
 		AutoValidate auto_validate = AutoValidate.Inherit;
 
 		[Browsable (false)]
