febc34a345b5a3156ab7142be8ecdc212e196d90
Jiri Volejnik <volejnik@emclient.com>
2017-04-19 16:46:38 +0000

    Mac: native calendar control in popover used in standard DateTimePicker

---
+++ mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/Extensions.cs
@@ -0,0 +1,24 @@
+﻿#if MONOMAC
+using MonoMac.Foundation;
+#elif XAMARINMAC
+using Foundation;
+#endif
+
+namespace System.Windows.Forms.Mac
+{
+	public static class Extensions
+	{
+		static DateTime reference = new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+		public static NSDate ToNSDate(this DateTime datetime)
+		{
+			return NSDate.FromTimeIntervalSinceReferenceDate((datetime.ToUniversalTime() - reference).TotalSeconds);
+		}
+
+		public static DateTime ToDateTime(this NSDate date)
+		{
+			return reference.AddSeconds(date.SecondsSinceReferenceDate).ToLocalTime();
+		}
+
+	}
+}
