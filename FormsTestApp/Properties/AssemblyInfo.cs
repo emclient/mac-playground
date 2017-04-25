b5cc8cdaec51db4f2be61be92c5a6108db3e3592
Jiri Volejnik <aconcagua21@volny.cz>
2017-04-25 13:07:40 +0000

    Mac: FormsTestApp added. OSXPlayground solution added to the root folder.

---
+++ FormsTestApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// AssemblyTitle, AssemblyProduct and AssemblyCopyright are in BrandingUtils.cs
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: InternalsVisibleTo("MailClient.UITest")]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7625efa3-2b03-4dee-9c07-3cdcdd5122a4")]
+
+// NGEN optimization
+[assembly: Dependency("MailClient", LoadHint.Always)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("7.1.29943.1")]
+[assembly: AssemblyFileVersion("7.1.29943.1")]
