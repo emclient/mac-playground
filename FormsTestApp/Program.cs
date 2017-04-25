b59dd4cd3278fd6912375a82f5ba238c80e13db6
Jiri Volejnik <volejnik@emclient.com>
2017-04-25 13:07:40 +0000

    Mac: FormsTestApp added. OSXPlayground solution added to the root folder.

---
+++ FormsTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("FormsTest")]
+[assembly: AssemblyProduct("FormsTest")]
+[assembly: AssemblyCopyright("Copyright © 2006-2017 eM Client s.r.o.")]
+[assembly: AssemblyCompany("eM Client s.r.o.")]
+
+namespace FormsTestApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			FormsTest.Program.Main(args);
+		}
+	}
+}
