24d91cd3823c03f3bc28b4017d0312a50bcf04dd
Tomas Bambuch <bambuch@emclient.com>
2017-05-31 11:57:18 +0000

    add Uniscribe ScriptGetProperties function (should be used later for checking whether the script is a complex script)

---
+++ MacBridge/WinApi/Usp10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WinApi
+{
+	public static partial class Win32
+	{
+		public static bool ScriptGetProperties([Out] out IntPtr ppSp, [Out] out int piNumScripts)
+		{
+			ppSp = IntPtr.Zero;
+			piNumScripts = 0;
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return true;
+		}
+	}
+}
