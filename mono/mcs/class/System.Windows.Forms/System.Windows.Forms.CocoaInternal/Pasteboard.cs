53b70d46fd4a529c921b853e2bbb90c1f34c4596
Jiri Volejnik <volejnik@emclient.com>
2017-09-11 14:50:51 +0000

    Mac: Copy & Paste HTML and RTF formats from and to a message.

--- mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/Pasteboard.cs
+++ mono/mcs/class/System.Windows.Forms/System.Windows.Forms.CocoaInternal/Pasteboard.cs
@@ -82,16 +82,14 @@ namespace System.Windows.Forms.CocoaInternal {
 		internal static readonly string internal_format;
 		internal static readonly string serialized_format;
 
-		internal const string fmt_Text = "Text";
-		internal const string fmt_UnicodeText = "UnicodeText";
 		internal const string fmt_public_utf8_plain_text = "public.utf8-plain-text";
-		internal const string fmt_HTML = "HTML Format";
-
-		internal const string NSPasteboardTypeHTML = "public.html";
 
 		internal const string NSDocumentTypeDocumentAttribute = "DocumentType";
+		internal const string NSCharacterEncodingDocumentAttribute = "CharacterEncoding";
+		internal const string NSHTMLTextDocumentType = "NSHTML";
 		internal const string NSRTFTextDocumentType = "NSRTF";
 		internal const string kUTTypeRTF = "public.rtf";
+		internal const string NSPasteboardTypeHTML = "public.html";
 
 		static Pasteboard ()
 		{
@@ -109,9 +107,13 @@ namespace System.Windows.Forms.CocoaInternal {
 			var name = DataFormats.GetFormat(id)?.Name;
 			switch (name)
 			{
-				// TODO: Add support for other typess
-				case fmt_Text:
+				// TODO: Add support for other types
+				case DataFormats.Text:
 					return pboard.GetStringForType(fmt_public_utf8_plain_text);
+				case DataFormats.Html:
+					return GetHTML(pboard);
+				case DataFormats.Rtf:
+					return GetRTF(pboard);
 			}
 
 			return null;
@@ -129,16 +131,41 @@ namespace System.Windows.Forms.CocoaInternal {
 			switch (name)
 			{
 				// TODO: Add support for other types
-				case fmt_Text:
+				case DataFormats.Text:
 					pboard.SetStringForType(data.ToString(), fmt_public_utf8_plain_text);
 					break;
-				case fmt_HTML:
+				case DataFormats.Html:
 					SetHTMLData(pboard, data);
 					break;
 			}
 		}
 
-		private static void SetHTMLData(NSPasteboard pboard, object data)
+		static object GetHTML(NSPasteboard pboard)
+		{
+			var data = pboard.GetDataForType(NSPasteboardTypeHTML);
+			if (data != null)
+			{
+				var html = data.ToString(NSStringEncoding.UTF8);
+				if (html == null)
+					html = data.ToString(NSStringEncoding.Unicode);
+				if (html != null)
+				{
+					var mshtml = AddMSClipboardMetadata(html.ToString());
+					return new MemoryStream(Encoding.UTF8.GetBytes(mshtml));
+				}
+			}
+			return null;
+		}
+
+		static object GetRTF(NSPasteboard pboard)
+		{
+			var data = pboard.GetDataForType(kUTTypeRTF);
+			if (data != null)
+				return data.ToString(NSStringEncoding.UTF8).ToString();
+			return null;
+		}
+
+		static void SetHTMLData(NSPasteboard pboard, object data)
 		{
 			if (data is MemoryStream stream)
 			{
@@ -151,15 +178,21 @@ namespace System.Windows.Forms.CocoaInternal {
 						s = s.Substring(index);
 
 					// Add as HTML
-					var nsdata = NSData.FromString(s, NSStringEncoding.Unicode);
+					//var nsdata = NSData.FromString(s, NSStringEncoding.Unicode);
+					var nsdata = NSData.FromString(s, NSStringEncoding.UTF8);
 					pboard.SetDataForType(nsdata, NSPasteboardTypeHTML);
 
 					// Add as RTF
-					var options = new NSDictionary();
+					nsdata = NSData.FromString(s, NSStringEncoding.UTF8);
+					var options = new NSMutableDictionary();
+					options[(NSString)NSDocumentTypeDocumentAttribute] = (NSString)NSHTMLTextDocumentType;
+					options[(NSString)NSCharacterEncodingDocumentAttribute] = new NSNumber((ulong)NSStringEncoding.UTF8);
+					var rtf = new NSAttributedString(nsdata, options, out NSDictionary attributes, out NSError error);
+
+					options.Clear();
 					options[(NSString)NSDocumentTypeDocumentAttribute] = (NSString)NSRTFTextDocumentType;
-					var rtf = new NSAttributedString(s, options, out NSDictionary attributes, out NSError error);
 					nsdata = rtf.GetData(new NSRange(0, rtf.Length), options, out error);
-					pboard.SetDataForType(nsdata,kUTTypeRTF);
+					pboard.SetDataForType(nsdata, kUTTypeRTF);
 				}
 			}
 		}
@@ -281,11 +314,19 @@ namespace System.Windows.Forms.CocoaInternal {
 
 		internal static void AppendTypeIDs(NSPasteboard pboard, List<int> ids, string type)
 		{
+			var usedIDs = new HashSet<string>();
+
 			switch (type)
 			{
 				// TODO: Add more identifiers - for images, sounds etc.
-				case "public.utf8-plain-text":
+				case fmt_public_utf8_plain_text:
 					ids.Add(DataFormats.Format.Add(DataFormats.Text).Id);
+					break;
+				case NSPasteboardTypeHTML:
+					ids.Add(DataFormats.Format.Add(DataFormats.Html).Id);
+					break;
+				case kUTTypeRTF:
+					ids.Add(DataFormats.Format.Add(DataFormats.Rtf).Id);
 					break;
 			}
 
@@ -301,5 +342,106 @@ namespace System.Windows.Forms.CocoaInternal {
 			get { return primary_pbref; }
 			//get { return app_pbref; }
 		}
+
+		internal static string AddMSClipboardMetadata(string html)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine(Header);
+			sb.AppendLine(@"<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.0 Transitional//EN"">");
+
+			// if given html already provided the fragments we won't add them
+			int fragmentStart, fragmentEnd;
+			int fragmentStartIdx = html.IndexOf(StartFragment, StringComparison.OrdinalIgnoreCase);
+			int fragmentEndIdx = html.LastIndexOf(EndFragment, StringComparison.OrdinalIgnoreCase);
+
+			// if html tag is missing add it surrounding the given html (critical)
+			int htmlOpenIdx = html.IndexOf("<html", StringComparison.OrdinalIgnoreCase);
+			int htmlOpenEndIdx = htmlOpenIdx > -1 ? html.IndexOf('>', htmlOpenIdx) + 1 : -1;
+			int htmlCloseIdx = html.LastIndexOf("</html", StringComparison.OrdinalIgnoreCase);
+
+			if (fragmentStartIdx < 0 && fragmentEndIdx < 0)
+			{
+				int bodyOpenIdx = html.IndexOf("<body", StringComparison.OrdinalIgnoreCase);
+				int bodyOpenEndIdx = bodyOpenIdx > -1 ? html.IndexOf('>', bodyOpenIdx) + 1 : -1;
+
+				if (htmlOpenEndIdx < 0 && bodyOpenEndIdx < 0)
+				{
+					// the given html doesn't contain html or body tags so we need to add them and place start/end fragments around the given html only
+					sb.Append("<html><body>");
+					sb.Append(StartFragment);
+					fragmentStart = GetByteCount(sb);
+					sb.Append(html);
+					fragmentEnd = GetByteCount(sb);
+					sb.Append(EndFragment);
+					sb.Append("</body></html>");
+				}
+				else
+				{
+					// insert start/end fragments in the proper place (related to html/body tags if exists) so the paste will work correctly
+					int bodyCloseIdx = html.LastIndexOf("</body", StringComparison.OrdinalIgnoreCase);
+
+					if (htmlOpenEndIdx < 0)
+						sb.Append("<html>");
+					else
+						sb.Append(html, 0, htmlOpenEndIdx);
+
+					if (bodyOpenEndIdx > -1)
+						sb.Append(html, htmlOpenEndIdx > -1 ? htmlOpenEndIdx : 0, bodyOpenEndIdx - (htmlOpenEndIdx > -1 ? htmlOpenEndIdx : 0));
+
+					sb.Append(StartFragment);
+					fragmentStart = GetByteCount(sb);
+
+					var innerHtmlStart = bodyOpenEndIdx > -1 ? bodyOpenEndIdx : (htmlOpenEndIdx > -1 ? htmlOpenEndIdx : 0);
+					var innerHtmlEnd = bodyCloseIdx > -1 ? bodyCloseIdx : (htmlCloseIdx > -1 ? htmlCloseIdx : html.Length);
+					sb.Append(html, innerHtmlStart, innerHtmlEnd - innerHtmlStart);
+
+					fragmentEnd = GetByteCount(sb);
+					sb.Append(EndFragment);
+
+					if (innerHtmlEnd < html.Length)
+						sb.Append(html, innerHtmlEnd, html.Length - innerHtmlEnd);
+
+					if (htmlCloseIdx < 0)
+						sb.Append("</html>");
+				}
+			}
+			else
+			{
+				// handle html with existing start\end fragments just need to calculate the correct bytes offset (surround with html tag if missing)
+				if (htmlOpenEndIdx < 0)
+					sb.Append("<html>");
+				int start = GetByteCount(sb);
+				sb.Append(html);
+				fragmentStart = start + GetByteCount(sb, start, start + fragmentStartIdx) + StartFragment.Length;
+				fragmentEnd = start + GetByteCount(sb, start, start + fragmentEndIdx);
+				if (htmlCloseIdx < 0)
+					sb.Append("</html>");
+			}
+
+			// Back-patch offsets (scan only the header part for performance)
+			sb.Replace("<<<<<<<<1", Header.Length.ToString("D9"), 0, Header.Length);
+			sb.Replace("<<<<<<<<2", GetByteCount(sb).ToString("D9"), 0, Header.Length);
+			sb.Replace("<<<<<<<<3", fragmentStart.ToString("D9"), 0, Header.Length);
+			sb.Replace("<<<<<<<<4", fragmentEnd.ToString("D9"), 0, Header.Length);
+
+			return sb.ToString();
+		}
+
+		static int GetByteCount(StringBuilder sb, int start = 0, int end = -1)
+		{
+			int count = 0;
+			char[] byteCount = new char[1];
+			end = end > -1 ? end : sb.Length;
+			for (int i = start; i < end; i++)
+			{
+				byteCount[0] = sb[i];
+				count += Encoding.UTF8.GetByteCount(byteCount);
+			}
+			return count;
+		}
+
+		const string Header = "Version:0.9\n\r\nStartHTML:<<<<<<<<1\r\nEndHTML:<<<<<<<<2\r\nStartFragment:<<<<<<<<3\r\nEndFragment:<<<<<<<<4\r\nStartSelection:<<<<<<<<3\r\nEndSelection:<<<<<<<<4";
+		const string StartFragment = "<!--StartFragment-->";
+		const string EndFragment = @"<!--EndFragment-->";
 	}
 }
