13b84f06219872e1f6d472b8a77f44805e6c33ea
Jiri Volejnik <volejnik@emclient.com>
2017-11-20 14:19:08 +0000

    Mac: FormsTest updated (edit box, System.Net.Logging reflection). Todo updated.

--- FormsTest/TextBoxForm.cs
+++ FormsTest/TextBoxForm.cs
@@ -20,5 +20,30 @@ namespace FormsTest
             // Mac layout debugging
             //this.DebugAllControls();
         }
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			var systemAssembly = typeof(System.Net.WebClient).Assembly;
+			var loggingType = systemAssembly.GetType("System.Net.Logging", false);
+
+			var nonPublicStaticProperties = loggingType.GetMembers(System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+			var nonPublicStaticFields = loggingType.GetMembers(System.Reflection.BindingFlags.SetField | System.Reflection.BindingFlags.GetField | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+			var nonPublicStaticMethods = loggingType.GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+			textbox4.AppendText("\n\nNon Public Static Properties:");
+			foreach (var p in nonPublicStaticProperties)
+				textbox4.AppendText($"\n{p.Name}");
+
+			textbox4.AppendText("\n\nNon Public Static Fileds:");
+			foreach (var f in nonPublicStaticFields)
+				textbox4.AppendText($"\n{f.Name}");
+
+			textbox4.AppendText("\n\nNon Public Static Methods:");
+			foreach (var m in nonPublicStaticMethods)
+				textbox4.AppendText($"\n{m.Name}()");
+
+		}
     }
 }
