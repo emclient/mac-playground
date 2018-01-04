e06b0e86d81f60c592a0c98e8126a00222667ce5
Jiri Volejnik <volejnik@emclient.com>
2018-01-04 09:47:23 +0000

    Mac: Activate apps (DbRepair, DbBackup, eM Client from DbBackup) after launch, so that they appear front, not behind other apps. Do not execute DbBackup directly from button click handler, as it blocks UI.

---
+++ MacBridge/MacApi/AppKit/NSApplication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+#if MONOMAC
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+#elif XAMARINMAC
+using Foundation;
+using AppKit;
+#endif
+
+namespace MacBridge.AppKit
+{
+	public static class NSApplicationEx
+	{
+		public static bool Activate(int pid, int wait = 5000)
+		{
+			NSRunningApplication app = null;
+			for (; wait > 0; wait -= 100)
+			{
+				if (app == null)
+					app = NSRunningApplication.GetRunningApplication(pid);
+
+				if (app != null)
+					if (app.Activate(NSApplicationActivationOptions.ActivateIgnoringOtherWindows))
+						return true;
+					
+				System.Threading.Thread.Sleep(100);
+			}
+			return false;
+		}
+
+		public static void ActivateCurrent()
+		{
+			NSApplication.SharedApplication.ActivateIgnoringOtherApps(true);
+		}
+	}
+}
---
+++ MacBridge/MacApi/AppKit/NSWorkspace.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+#if MONOMAC
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+#elif XAMARINMAC
+using Foundation;
+using AppKit;
+#endif
+
+namespace MacBridge.AppKit
+{
+    public static class NSWorkspaceEx
+    {
+        public static int LaunchApplicationForPath(string path, string[] args, bool activate = true)
+        {
+            var options = new NSWorkspaceLaunchOptions();
+            var configuration = new NSDictionary();
+			var encodedPath = System.Net.WebUtility.UrlEncode(path);
+            var url = new NSUrl("file://" + encodedPath);
+
+            NSError error = null;
+#if MONOMAC
+            var app = NSWorkspace.SharedWorkspace.LaunchApplication(url, options, configuration, error);
+#else
+            var app = NSWorkspace.SharedWorkspace.OpenURL(url, options, configuration, out error);
+#endif
+            if (error != null)
+                throw new ApplicationException("NSWorkspace failed to open URL: " + url);
+
+            if (app == null)
+                return 0;
+
+            if (activate)
+                app.Activate(NSApplicationActivationOptions.ActivateIgnoringOtherWindows);
+
+            return app.ProcessIdentifier;
+        }
+    }
+}
