c2687e78f696ae71acc8458fadfe6919eece9bb8
Jiri Volejnik <aconcagua21@volny.cz>
2018-01-05 08:38:40 +0000

    Mac: Launching DbRepair, DbBackup and relaunching eM Client from DbBackup modified so that it uses NSWorkspace. This is currently the only reliable way to launch the app in front of all the other apps. However, we cannot get the exit code this way. It's only possible using NSTask (child process).

--- MacBridge/MacApi/AppKit/NSWorkspace.cs
+++ MacBridge/MacApi/AppKit/NSWorkspace.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
 
 #if MONOMAC
 using MonoMac.Foundation;
@@ -12,14 +15,19 @@ namespace MacBridge.AppKit
 {
     public static class NSWorkspaceEx
     {
-        public static int LaunchApplicationForPath(string path, string[] args, bool activate = true)
+		public static int LaunchApplicationForPath(string path, string args, bool activate = true)
+		{
+			return LaunchApplicationForPath(path, SplitArgs(args), activate);
+		}
+
+		static int LaunchApplicationForPath(string path, string[] args, bool activate = true)
         {
             var options = new NSWorkspaceLaunchOptions();
-            var configuration = new NSDictionary();
-			var encodedPath = System.Net.WebUtility.UrlEncode(path);
-            var url = new NSUrl("file://" + encodedPath);
-
-            NSError error = null;
+			var arguments = NSArray.FromObjects(args);
+            var configuration = NSDictionary.FromObjectAndKey(arguments, NSWorkspace.LaunchConfigurationArguments);
+			var url = new NSUrl(path, false);
+            
+			NSError error = null;
 #if MONOMAC
             var app = NSWorkspace.SharedWorkspace.LaunchApplication(url, options, configuration, error);
 #else
@@ -36,5 +44,69 @@ namespace MacBridge.AppKit
 
             return app.ProcessIdentifier;
         }
+
+		public static int LaunchAppAndWaitForExit(string path, string args, bool activate = true)
+		{
+			return LaunchAppAndWaitForExit(path, SplitArgs(args), activate);
+		}
+
+		static int LaunchAppAndWaitForExit(string path, string[] args, bool activate = true)
+		{
+			var pid = LaunchApplicationForPath(path, args, activate);
+			var process = System.Diagnostics.Process.GetProcessById(pid);
+			process.WaitForExit();
+			return 0; //process.ExitCode; ExitCode not implemented in Mono
+		}
+
+		public static NSTask LaunchTaskForPath(string path, string[] args, bool activate = true)
+		{
+			var task = new NSTask();
+			var pipe = new NSPipe();
+
+			task.StandardOutput = pipe;
+			task.LaunchPath = path;
+			task.Arguments = args;
+			task.Launch();
+
+			if (activate)
+				NSApplicationEx.Activate(task.ProcessIdentifier);
+
+			return task;
+		}
+
+		public static int LaunchTaskAndWaitForExit(string path, string args)
+		{
+			return LaunchTaskAndWaitForExit(path, SplitArgs(args));
+	
+		}
+
+		static int LaunchTaskAndWaitForExit(string path, string[] args)
+		{
+			var task = LaunchTaskForPath(path, args);
+			task.WaitUntilExit();
+			return task.TerminationStatus;
+		}
+
+		static Regex SplitArgsRegex = new Regex("(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+		static string[] SplitArgs(string args)
+		{
+			var split = SplitArgsRegex.Split(args);
+			var list = new List<String>();
+			foreach (var arg in split)
+			{
+				var unwrapped = Unwrap(arg);
+				if (unwrapped.Length != 0)
+					list.Add(unwrapped);
+			}
+			return list.ToArray();
+		}
+
+		static string Unwrap(string s, string prefix="\"", string suffix = "\"")
+		{
+			if (s.Length > prefix.Length+suffix.Length && s.StartsWith(prefix) && s.EndsWith(suffix))
+			    return s.Substring(prefix.Length, s.Length - prefix.Length - suffix.Length);
+			return s;
+		}
     }
 }
