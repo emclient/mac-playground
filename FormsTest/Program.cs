5a5b0d30390128fe115ead7f9e79387b259aa878
Jiri Volejnik <volejnik@emclient.com>
2018-01-23 16:57:06 +0000

    Mac: open/fopen/FileStream experimenst added to FormsTest

--- FormsTest/Program.cs
+++ FormsTest/Program.cs
@@ -5,6 +5,9 @@ using FormsTest;
 using System.Diagnostics;
 using MailClient.Common.UI;
 using Xilium.CefGlue;
+using MacBridge.Posix;
+using System.IO;
+using System.Runtime.InteropServices;
 
 #if MAC
 
@@ -36,20 +39,22 @@ namespace FormsTest
 			}
 		}
 
-        /// <summary>
+		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		public static void Main(string[] args)
 		{
-            Application.EnableVisualStyles();
+			MaxOpenFiles();
+
+			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.ApplicationExit += (object sender, EventArgs e) =>
 			{
-                //UrlProtocol.Unregister();
+				//UrlProtocol.Unregister();
 			};
 
-            //UrlProtocol.Register();
+			//UrlProtocol.Register();
 
 			Marshalling.Initialize();
 			//CefApp.
@@ -58,10 +63,58 @@ namespace FormsTest
 
 			var threads = Process.GetCurrentProcess().Threads;
 
+			Terminate();
+		}
+
 #if MAC
+		static void Terminate()
+		{
 			// Workaround for not exiting the app because of pending threads related to UrlProtocol subclass.
-            NSApplication.SharedApplication.Terminate(NSApplication.SharedApplication);
-#endif
-        }
+			NSApplication.SharedApplication.Terminate(NSApplication.SharedApplication);
+		}
+
+		static void MaxOpenFiles()
+		{
+			var lim = new LibC.rlimit();
+			LibC.getrlimit((int)LibC.RLimit.NoFile, ref lim);
+
+			int n = 9000;
+			lim.cur = lim.max = (UInt64)n;
+			LibC.setrlimit((int)LibC.RLimit.NoFile, ref lim);
+
+			LibC.getrlimit((int)LibC.RLimit.NoFile, ref lim);
+
+			lim.cur = (UInt64)4000;
+			LibC.setrlimit((int)LibC.RLimit.NoFile, ref lim);
+
+			var a = new FileStream[n];
+			var f = Path.GetTempFileName();
+			int i = 0;
+			try
+			{
+				for (i = 0; i < n; ++i)
+				{
+					a[i] = new FileStream(f, FileMode.Open, FileAccess.Read);
+					//var result = LibC.open(f, LibC.FileMode.S_IRUSR);
+					//var result = LibC.fopen(f, "rb");
+					//if (result == IntPtr.Zero)
+						//break;
+				}
+			}
+			catch
+			{
+			}
+			Console.WriteLine($"#{i}");
+		}
+
 	}
+#else
+		static void Terminate()
+		{
+		}
+
+		static void MaxOpenFiles()
+		{
+		}
+#endif
 }
