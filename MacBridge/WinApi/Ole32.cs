6e7d4e17ca942ab8f7be3bf74460797b95034281
Filip Navara <navara@emclient.com>
2019-01-11 17:51:50 +0100

    Fix build

--- MacBridge/WinApi/Ole32.cs
+++ MacBridge/WinApi/Ole32.cs
@@ -5,27 +5,10 @@ using System.Runtime.InteropServices.ComTypes;
 
 namespace WinApi
 {
-    public static partial class Win32
-    {
-        public static int RevokeDragDrop(IntPtr hwnd)
-        {
-			NotImplemented(MethodBase.GetCurrentMethod());
-			return 0;
-        }
-
-		public static IStream CreateStreamOnHGlobal(IntPtr hGlobal, [MarshalAs(UnmanagedType.Bool)] bool fDeleteOnRelease)
-		{
-			NotImplemented(MethodBase.GetCurrentMethod());
-			return null;
-		}
-
-		public static void DoDragDrop(IDataObject dataObject, IDropSource dropSource, int allowedEffects, int[] finalEffect)
-		{
-			NotImplemented(MethodBase.GetCurrentMethod());
-		}
-
-		public static int OleGetClipboard(ref System.Runtime.InteropServices.ComTypes.IDataObject data)
-		{
+	public static partial class Win32
+	{
+		public static int RevokeDragDrop(IntPtr hwnd)
+        	{
 			NotImplemented(MethodBase.GetCurrentMethod());
 			return 0;
 		}
