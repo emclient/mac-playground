61418c2578f0410d097c48365e73d829eb136d79
Filip Navara <filip.navara@gmail.com>
2019-01-11 19:52:46 +0000

    Fix build #3

--- MacBridge/WinApi/Ole32.cs
+++ MacBridge/WinApi/Ole32.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
@@ -8,7 +8,13 @@ namespace WinApi
 	public static partial class Win32
 	{
 		public static int RevokeDragDrop(IntPtr hwnd)
-        	{
+		{
+			NotImplemented(MethodBase.GetCurrentMethod());
+			return 0;
+		}
+
+		public static int OleGetClipboard(ref System.Runtime.InteropServices.ComTypes.IDataObject data)
+		{
 			NotImplemented(MethodBase.GetCurrentMethod());
 			return 0;
 		}
