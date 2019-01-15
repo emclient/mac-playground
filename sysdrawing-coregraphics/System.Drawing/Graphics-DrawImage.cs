b03c85b3d3fa400d97882ca29a4e890e760402a5
Jiri Volejnik <aconcagua@volny.cz>
2019-01-15 12:44:47 +0000

    Mac: Convert size from device to user space coordinates when drawing an image at a point.

--- sysdrawing-coregraphics/System.Drawing/Graphics-DrawImage.cs
+++ sysdrawing-coregraphics/System.Drawing/Graphics-DrawImage.cs
@@ -234,7 +234,8 @@ namespace System.Drawing
 				width = ConversionHelpers.GraphicsUnitConversion (GraphicsUnit.Pixel, graphicsUnit, image.HorizontalResolution, width);
 				height = ConversionHelpers.GraphicsUnitConversion (GraphicsUnit.Pixel, graphicsUnit, image.VerticalResolution, height);
 			}
-			DrawImage (image, new RectangleF(x,y,width,height));
+			var uss = context.ConvertSizeToUserSpace(new CGSize(width, height));
+			DrawImage(image, new RectangleF(x, y, (float)uss.Width, (float)uss.Height));
 		}
 
 		/// <summary>
