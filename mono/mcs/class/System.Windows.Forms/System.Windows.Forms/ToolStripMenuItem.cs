0beaeaa1b769bf01cd50f2125907d0a9a8f34505
Jiri Volejnik <volejnik@emclient.com>
2019-01-18 16:19:26 +0000

    Mac: Reverted blocked checking of top level control in ToolStripMenuItem. Fixes issues with shortcuts.

--- mono/mcs/class/System.Windows.Forms/System.Windows.Forms/ToolStripMenuItem.cs
+++ mono/mcs/class/System.Windows.Forms/System.Windows.Forms/ToolStripMenuItem.cs
@@ -400,11 +400,7 @@ namespace System.Windows.Forms
 			Control source = Control.FromHandle (m.HWnd);
 			Form f = source == null ? null : source.TopLevelControl as Form;
 
-			if (this.Enabled && keyData == this.shortcut_keys
-#if !(MONOMAC || XAMARINMAC) // ToolStrip can be embedded in ToolbarAccessoryView
-				&& GetTopLevelControl () == f
-#endif
-				)
+			if (this.Enabled && keyData == this.shortcut_keys && GetTopLevelControl () == f)
 			{
 				this.FireEvent (EventArgs.Empty, ToolStripItemEventType.Click);
 				return true;
