6d0ce6580b7bc2609e154bfaad20532b7e81083b
Jiri Volejnik <aconcagua@volny.cz>
2019-07-27 19:41:51 +0000

    Mac: SaveFileDialog enhancements (support for multiple extensions, extension hiding and extension overwriting). Added .asc file extension definition to plist. Addresses issue https://emclient.atlassian.net/browse/EC-2735 (Export PGP key issues).

--- mono/mcs/class/System.Windows.Forms/System.Windows.Forms/SaveFileDialog.Mac.cs
+++ mono/mcs/class/System.Windows.Forms/System.Windows.Forms/SaveFileDialog.Mac.cs
@@ -4,6 +4,8 @@ using System;
 using System.Windows.Forms;
 using System.Windows.Forms.CocoaInternal;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 #if XAMARINMAC
 using AppKit;
@@ -26,7 +28,7 @@ namespace System.Windows.Forms
 		public string SelectedPath { get; set; }
 
 		// Gets or sets a value indicating whether the New Folder button appears in the folder browser dialog box.
-		public bool ShowNewFolderButton	{ get; set; }
+		public bool ShowNewFolderButton { get; set; }
 
 		public override void Reset()
 		{
@@ -46,6 +48,8 @@ namespace System.Windows.Forms
 				{
 					var panel = new NSSavePanel();
 
+					panel.CanSelectHiddenExtension = true;
+
 					if (!String.IsNullOrWhiteSpace(InitialDirectory) && Directory.Exists(InitialDirectory))
 					{
 						panel.DirectoryUrl = NSUrl.FromFilename(InitialDirectory);
@@ -54,6 +58,9 @@ namespace System.Windows.Forms
 
 					if (!String.IsNullOrWhiteSpace(FileName))
 						panel.NameFieldStringValue = FileName;
+
+					if (!String.IsNullOrEmpty(Filter))
+						ApplyFilter(panel, Filter);
 
 					if (NSPanelButtonType.Ok != (NSPanelButtonType)(int)panel.RunModal())
 						return false;
@@ -87,6 +94,36 @@ namespace System.Windows.Forms
 			}
 
 			return retValue;
+		}
+
+		internal void ApplyFilter(NSSavePanel panel, string filter)
+		{
+			var interlaced = filter.Split('|');
+			if (interlaced.Length == 0)
+				return;
+
+			var groups = interlaced.Where((value, index) => index % 2 != 0).ToList();
+			var extensions = new List<string>();
+			foreach (var group in groups)
+			{
+				var items = group.Split(';');
+				foreach (var item in items)
+				{
+					var position = item.LastIndexOf('.');
+					var extension = position < 0 ? item : item.Substring(1 + position);
+
+					if (!String.IsNullOrWhiteSpace(extension))
+					{
+						if ("*".Equals(extension, StringComparison.InvariantCulture))
+							panel.AllowsOtherFileTypes = true;
+						else
+							extensions.Add(extension);
+					}
+				}
+			}
+
+			if (extensions.Count != 0)
+				panel.AllowedFileTypes = extensions.ToArray();
 		}
 	}
 }
