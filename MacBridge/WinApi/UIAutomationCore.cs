92b261118f23641c71a55b8c0ffe8a290acbb58c
volejnik <jirkav@172-4-1-30.lightspeed.rcsntx.sbcglobal.net>
2020-03-18 18:16:06 +0100

    Extracting UIA apis into WinApi

--- MacBridge/WinApi/UIAutomationCore.cs
+++ MacBridge/WinApi/UIAutomationCore.cs
@@ -1,10 +1,67 @@
 using System;
-namespace MacBridge.WinApi
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+
+namespace WinApi
 {
-	public class UIAutomationCore
+	public static partial class Win32
 	{
-		public UIAutomationCore()
+		public static IntPtr UiaReturnRawElementProvider(IntPtr hwnd, IntPtr wParam, IntPtr lParam, IRawElementProviderSimple provider)
 		{
+			return IntPtr.Zero;
+		}
+
+		public static int UiaHostProviderFromHwnd(IntPtr handle, out IRawElementProviderSimple host)
+		{
+			host = null;
+			return 0;
+		}
+
+		public static int UiaGetReservedNotSupportedValue(out object notSupportedValue)
+		{
+			notSupportedValue = null;
+			return 0;
+		}
+
+		public static int UiaRaiseAutomationPropertyChangedEvent(IRawElementProviderSimple provider, int id, object oldValue, object newValue)
+		{
+			return 0;
+		}
+
+		public static int UiaRaiseAutomationEvent(IRawElementProviderSimple provider, int id)
+		{
+			return 0;
+		}
+
+		public static int UiaRaiseStructureChangedEvent(IRawElementProviderSimple provider, StructureChangeType structureChangeType, int[] runtimeId, int runtimeIdLen)
+		{
+			return 0;
+		}
+
+		public static int UiaRaiseAsyncContentLoadedEvent(IRawElementProviderSimple provider, AsyncContentLoadedState asyncContentLoadedState, double PercentComplete)
+		{
+			return 0;
+		}
+
+		public static bool UiaClientsAreListening()
+		{
+			return false;
+		}
+
+		public static bool UiaDisconnectProvider(IRawElementProviderSimple provider)
+		{
+			return true;
+		}
+
+		public static int UiaLookupId(AutomationIdentifierType type, ref Guid guid)
+		{
+			return 0;
+		}
+
+		public static int UiaGetReservedMixedAttributeValue(out object mixedAttributeValue)
+		{
+			mixedAttributeValue = null;
+			return 0;
 		}
 	}
 }
