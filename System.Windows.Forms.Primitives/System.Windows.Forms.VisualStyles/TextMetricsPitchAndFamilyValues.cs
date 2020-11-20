91119bf0147e1d5ac20f734b59800787652265f5
Filip Navara <navara@emclient.com>
2020-11-20 18:06:00 +0100

    Add System.Windows.Forms.Primitives implementation and use it from System.Windows.Forms

---
+++ System.Windows.Forms.Primitives/System.Windows.Forms.VisualStyles/TextMetricsPitchAndFamilyValues.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Windows.Forms.VisualStyles
+{
+    [Flags]
+    public enum TextMetricsPitchAndFamilyValues
+    {
+        FixedPitch = 1,
+        Vector = 2,
+        TrueType = 4,
+        Device = 8
+    }
+}
