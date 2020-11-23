c69eeec860cf2f4bf0867a3dc9c68cb463de0d27
Filip Navara <navara@emclient.com>
2020-11-23 11:32:52 +0100

    Fix attributes on FolderBrowserDialog

--- System.Windows.Forms/System.Windows.Forms/FolderBrowserDialog.cs
+++ System.Windows.Forms/System.Windows.Forms/FolderBrowserDialog.cs
@@ -751,25 +751,6 @@ namespace System.Windows.Forms {
 			}
 		}
 	}
-	
-	internal class SpecialFolderEnumConverter : TypeConverter
-	{
-		public override object ConvertFrom (ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
-		{
-			if ((value == null) || !(value is String))
-				return base.ConvertFrom (context, culture, value);
-
-			return Enum.Parse (typeof (Environment.SpecialFolder), (string)value, true);
-		}
-
-		public override object ConvertTo (ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
-		{
-			if ((value == null) || !(value is Environment.SpecialFolder) || (destinationType != typeof (string)))
-				return base.ConvertTo (context, culture, value, destinationType);
-				
-			return ((Environment.SpecialFolder)value).ToString ();
-		}
-	}
 }
 
 #endif // MONOMAC
