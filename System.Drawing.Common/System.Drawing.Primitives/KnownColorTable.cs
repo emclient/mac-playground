2ef414a66205bcff6c2a9580ec16ca85b0761a09
volejnik <volejnik@emclient.com>
2021-04-15 09:46:31 +0200

    Revert "Mac: Restore support for dynamic system colors. Fixis issues with invisible text and black background in text boxes in Dark Theme under system Light Mode. Addresses EC-5292."
    This reverts commit a4d91610fd44193d8246dd48caa1ec72d0d6d9c6.

--- System.Drawing.Common/System.Drawing.Primitives/KnownColorTable.cs
+++ System.Drawing.Common/System.Drawing.Primitives/KnownColorTable.cs
@@ -2,12 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
-using System.Drawing.Mac;
-#if __MACOS__
-using AppKit;
-#elif __IOS__
-using UIKit;
-#endif
 
 namespace System.Drawing
 {
@@ -291,25 +285,10 @@ namespace System.Drawing
         {
             Debug.Assert(Color.IsKnownColorSystem(color));
 
-            return color < KnownColor.Transparent ?
-#if __MACOS__
-                 GetNativeColorArgb(color) ??
-#endif
-                s_staticSystemColors[(int)color - (int)KnownColor.ActiveBorder]
+            return color < KnownColor.Transparent
+                ? s_staticSystemColors[(int)color - (int)KnownColor.ActiveBorder]
                 : s_staticSystemColors[(int)color - (int)KnownColor.ButtonFace + (int)KnownColor.WindowText];
         }
-
-#if __MACOS__
-        // Certain system colors are dynamic - having different value in Dark Mode then in Light Mode. They don't have to be opaque.
-        public static uint? GetNativeColorArgb(KnownColor kc)
-        {
-            var rgb = Color.FromKnownColor(kc).ToNSColor(false)?.UsingColorSpace(NSColorSpace.DeviceRGBColorSpace);
-            if (rgb != null && rgb.ToArgb(out int a, out int r, out int g, out int b))
-                return (uint)((int)((uint)a << 24) + (r << 16) + (g << 8) + b);
-            return null;
-        }
 #endif
-
-#endif //FEATURE_WINDOWS_SYSTEM_COLORS
     }
 }
