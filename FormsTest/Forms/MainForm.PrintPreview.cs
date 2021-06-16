f0f08ad8fc70755c5e6fb43fcbfa16b3bf6aba24
volejnik <volejnik@emclient.com>
2021-06-16 13:01:03 +0200

    Mac: PrintPreview experiment skeleton added to FormsTest

---
+++ FormsTest/Forms/MainForm.PrintPreview.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace FormsTest
+{
+	public partial class MainForm
+	{
+		PrintPreviewDialog printPreviewDialog;
+		PrintDocument printDocument;
+        string printDocumentContents;
+
+        public void PrintPreview()
+		{
+            printDocumentContents = "This is to be printed\nLine 2\nAnotherLine";
+
+			printDocument = new PrintDocument();
+            printDocument.PrintPage += printDocument_PrintPage;
+
+            printPreviewDialog = new PrintPreviewDialog();
+            printPreviewDialog.Document = printDocument;
+            printPreviewDialog.ShowDialog();
+        }
+
+        void printDocument_PrintPage(object sender, PrintPageEventArgs e)
+		{
+            var stringToPrint = printDocumentContents;
+
+            int charactersOnPage = 0;
+            int linesPerPage = 0;
+
+            // Sets the value of charactersOnPage to the number of characters
+            // of stringToPrint that will fit within the bounds of the page.
+            e.Graphics.MeasureString(stringToPrint, this.Font,
+                e.MarginBounds.Size, StringFormat.GenericTypographic,
+                out charactersOnPage, out linesPerPage);
+
+            // Draws the string within the bounds of the page.
+            e.Graphics.DrawString(stringToPrint, this.Font, Brushes.Black,
+            e.MarginBounds, StringFormat.GenericTypographic);
+
+            // Remove the portion of the string that has been printed.
+            stringToPrint = stringToPrint.Substring(charactersOnPage);
+
+            // Check to see if more pages are to be printed.
+            e.HasMorePages = (stringToPrint.Length > 0);
+
+            // If there are no more pages, reset the string to be printed.
+            if (!e.HasMorePages)
+                stringToPrint = printDocumentContents;
+
+            printPreviewDialog.ShowDialog();
+        }
+    }
+}
